9e531e4ef84a529e8e4177a33dd60e2a81f558f5
seungjun <jjw5741@naver.com>
2025-06-29 16:50:11 +0900

    [mod/#4] 스테이터스바, 네비게이션바 투명화 설정
    - MainActivity에서 enableEdgeToEdge를 사용하여 상태바 및 네비게이션바 스타일을 투명하게 설정
    - Theme.kt에서 상태바 색상 관련 로직 제거

--- app/src/main/java/com/bongtu/baekseo/core/designsystem/theme/Theme.kt
+++ app/src/main/java/com/bongtu/baekseo/core/designsystem/theme/Theme.kt
@@ -1,13 +1,9 @@
 package com.bongtu.baekseo.core.designsystem.theme
 
-import android.app.Activity
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 object BongBaekTheme {
     val colors: BongBaekColors
@@ -42,17 +38,6 @@ fun BongBaekTheme(
         colors = defaultBongBaekColors,
         typography = defaultBongBaekTypography,
     ) {
-        val view = LocalView.current
-        if (!view.isInEditMode) {
-            SideEffect {
-                (view.context as Activity).window.run {
-                    WindowCompat.getInsetsController(
-                        this,
-                        view
-                    ).isAppearanceLightStatusBars = false
-                }
-            }
-        }
         MaterialTheme(
             content = content,
         )
--- app/src/main/java/com/bongtu/baekseo/presentation/main/MainActivity.kt
+++ app/src/main/java/com/bongtu/baekseo/presentation/main/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.bongtu.baekseo.presentation.main
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.bongtu.baekseo.core.designsystem.theme.BongBaekTheme
@@ -11,7 +13,10 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
+        )
         setContent {
             BongBaekTheme {
                 MainScreen()
