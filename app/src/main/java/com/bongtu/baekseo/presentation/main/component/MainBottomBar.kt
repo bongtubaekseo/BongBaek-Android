a974772310d85b334961c3426b5f54f9f0d61bf9
Jong <jm991014@gmail.com>
2025-07-01 20:39:40 +0900

    [ui/#3] Bottom Navigation Bar 구현

--- app/src/main/java/com/bongtu/baekseo/presentation/main/component/MainBottomBar.kt
+++ app/src/main/java/com/bongtu/baekseo/presentation/main/component/MainBottomBar.kt
@@ -5,25 +5,32 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideIn
 import androidx.compose.animation.slideOut
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color.Companion.Black
-import androidx.compose.ui.graphics.Color.Companion.Gray
-import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.role
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.bongtu.baekseo.core.util.NoRippleInteractionSource
+import com.bongtu.baekseo.core.designsystem.theme.BongBaekTheme
+import com.bongtu.baekseo.core.util.noRippleClickable
 import com.bongtu.baekseo.presentation.main.MainTab
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
@@ -34,53 +41,98 @@ fun MainBottomBar(
     tabs: ImmutableList<MainTab>,
     currentTab: MainTab?,
     onTabSelected: (MainTab) -> Unit,
+    modifier: Modifier = Modifier,
 ) {
     AnimatedVisibility(
         visible = isVisible,
         enter = fadeIn() + slideIn { IntOffset(0, 0) },
         exit = fadeOut() + slideOut { IntOffset(0, 0) },
+        modifier = modifier
+            .background(BongBaekTheme.colors.gray900),
     ) {
-        NavigationBar(containerColor = White) {
-            tabs.forEach { itemType ->
-                NavigationBarItem(
-                    interactionSource = NoRippleInteractionSource,
-                    selected = currentTab == itemType,
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            BongBaekTheme.colors.gray750,
+                            BongBaekTheme.colors.gray850,
+                        ),
+                    ),
+                    shape = RoundedCornerShape(
+                        topStart = 10.dp,
+                        topEnd = 10.dp,
+                    ),
+                )
+                .padding(
+                    top = 12.dp,
+                )
+                .navigationBarsPadding(),
+            horizontalArrangement = Arrangement.SpaceAround,
+        ) {
+            tabs.forEach { tab ->
+                val isSelected = tab == currentTab
+                val iconColor =
+                    if (isSelected) BongBaekTheme.colors.white
+                    else BongBaekTheme.colors.gray400
+                val textColor =
+                    if (isSelected) BongBaekTheme.colors.white
+                    else BongBaekTheme.colors.gray400
+
+                MainNavigationBarItem(
+                    tab = tab,
                     onClick = {
-                        onTabSelected(itemType)
+                        onTabSelected(tab)
                     },
-                    icon = {
-                        Icon(
-                            imageVector = ImageVector.vectorResource(itemType.iconRes),
-                            contentDescription = stringResource(itemType.title),
-                            modifier = Modifier.size(24.dp),
-                        )
-                    },
-                    label = {
-                        Text(
-                            text = stringResource(itemType.title),
-                            fontSize = 9.sp,
-                        )
-                    },
-                    colors = NavigationBarItemDefaults.colors(
-                        selectedIconColor = Black,
-                        selectedTextColor = Black,
-                        unselectedIconColor = Gray,
-                        unselectedTextColor = Gray,
-                        indicatorColor = White,
-                    )
+                    iconColor = iconColor,
+                    textColor = textColor,
                 )
             }
         }
     }
 }
 
+@Composable
+private fun MainNavigationBarItem(
+    tab: MainTab,
+    onClick: () -> Unit,
+    iconColor: Color,
+    textColor: Color,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .semantics(mergeDescendants = true) { role = Role.Tab }
+            .noRippleClickable(onClick),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+    ) {
+        Icon(
+            imageVector = ImageVector.vectorResource(tab.iconRes),
+            contentDescription = stringResource(tab.title),
+            tint = iconColor,
+            modifier = Modifier
+                .padding(horizontal = 8.dp),
+        )
+
+        Text(
+            text = stringResource(tab.title),
+            style = BongBaekTheme.typography.body2Regular14,
+            color = textColor,
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 private fun MainBottomBarPreview() {
-    MainBottomBar(
-        isVisible = true,
-        tabs = MainTab.entries.toImmutableList(),
-        currentTab = MainTab.DUMMY,
-        onTabSelected = {},
-    )
+    BongBaekTheme {
+        MainBottomBar(
+            isVisible = true,
+            tabs = MainTab.entries.toImmutableList(),
+            currentTab = MainTab.DUMMY,
+            onTabSelected = {},
+        )
+    }
 }
