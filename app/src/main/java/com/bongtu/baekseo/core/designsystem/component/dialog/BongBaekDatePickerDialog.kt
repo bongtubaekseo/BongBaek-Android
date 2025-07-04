7555cee0f2391f8bb673610256d175873e8b5b65
Hyejeong <myejeong0211@gmail.com>
2025-07-05 00:34:14 +0900

    [move/#7] date picker dialog 패키지 이동

--- app/src/main/java/com/bongtu/baekseo/core/designsystem/component/dialog/BongBaekDatePickerDialog.kt
+++ app/src/main/java/com/bongtu/baekseo/core/designsystem/component/dialog/BongBaekDatePickerDialog.kt
@@ -1,4 +1,4 @@
-package com.bongtu.baekseo.core.designsystem.component
+package com.bongtu.baekseo.core.designsystem.component.dialog
 
 import androidx.annotation.StringRes
 import androidx.compose.foundation.background
