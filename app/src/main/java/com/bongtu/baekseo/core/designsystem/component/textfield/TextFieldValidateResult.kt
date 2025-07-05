6d117dafa97a61fd13683ec17f4e80a4cedf9b9c
seungjun <jjw5741@naver.com>
2025-07-05 16:22:04 +0900

    [doc/#11] KDoc 작성

--- app/src/main/java/com/bongtu/baekseo/core/designsystem/component/textfield/TextFieldValidateResult.kt
+++ app/src/main/java/com/bongtu/baekseo/core/designsystem/component/textfield/TextFieldValidateResult.kt
@@ -2,6 +2,10 @@ package com.bongtu.baekseo.core.designsystem.component.textfield
 
 import androidx.compose.runtime.Immutable
 
+/**
+ *  TextField Validate Result
+ *  텍스트 필드의 유효성 검사 결과를 나타내는 sealed class
+ */
 @Immutable
 sealed class TextFieldValidateResult {
     abstract val message: String?
