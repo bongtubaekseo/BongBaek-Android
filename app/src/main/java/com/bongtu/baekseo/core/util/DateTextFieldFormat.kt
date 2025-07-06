1017608e868dbff41c3cf57b3abc973925d6e984
Hyejeong <myejeong0211@gmail.com>
2025-07-06 19:48:44 +0900

    [add/#21] date textfield format 추가

---
+++ app/src/main/java/com/bongtu/baekseo/core/util/DateTextFieldFormat.kt
@@ -0,0 +1,36 @@
+package com.bongtu.baekseo.core.util
+
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.OffsetMapping
+import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.text.input.VisualTransformation
+
+class DateTextFieldFormat : VisualTransformation {
+    override fun filter(text: AnnotatedString): TransformedText {
+        val input = text.text
+
+        if (input.length < 8) {
+            return TransformedText(AnnotatedString(""), OffsetMapping.Identity)
+        }
+
+        val month = input.substring(0, 2).toIntOrNull()
+        val day = input.substring(2, 4).toIntOrNull()
+        val year = input.substring(4, 8).toIntOrNull()
+
+        val formatted = buildString {
+            if (year != null) append("${year}년 ")
+            if (month != null) append("${month}월 ")
+            if (day != null) append("${day}일")
+        }
+
+        return TransformedText(AnnotatedString(formatted), object : OffsetMapping {
+            override fun originalToTransformed(offset: Int): Int {
+                return formatted.length
+            }
+
+            override fun transformedToOriginal(offset: Int): Int {
+                return input.length.coerceAtMost(8)
+            }
+        })
+    }
+}
