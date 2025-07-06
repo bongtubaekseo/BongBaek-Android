f00cec9594d71ce1c7d71ee81324fe148cde1670
seungjun <jjw5741@naver.com>
2025-07-06 23:02:54 +0900

    [fix/#20] RecordContract EventCategoryType 수정

--- app/src/main/java/com/bongtu/baekseo/presentation/record/RecordContract.kt
+++ app/src/main/java/com/bongtu/baekseo/presentation/record/RecordContract.kt
@@ -3,15 +3,16 @@ package com.bongtu.baekseo.presentation.record
 import androidx.compose.runtime.Immutable
 import com.bongtu.baekseo.core.common.state.UiState
 import com.bongtu.baekseo.core.common.type.EventType
-import com.bongtu.baekseo.data.model.Event
+import com.bongtu.baekseo.data.model.RecordEventItem
 import com.bongtu.baekseo.presentation.record.type.AttendType
+import com.bongtu.baekseo.presentation.record.type.EventCategoryType
 
 class RecordContract {
     @Immutable
     data class RecordState(
-        val recordLoadState: UiState<List<Event>> = UiState.Loading,
-        val attendType: String = AttendType.ATTEND.label,
-        val eventType: String = EventType.ALL.label,
+        val recordLoadState: UiState<List<RecordEventItem>> = UiState.Loading,
+        val attendType: AttendType = AttendType.ATTEND,
+        val eventCategoryType: EventCategoryType = EventCategoryType.ALL,
         val isDeleting: Boolean = false,
     )
 }
