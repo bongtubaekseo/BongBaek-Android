0a3caa6081b8d0806296e32a9fc84fecdbceb514
seungjun <jjw5741@naver.com>
2025-07-06 14:21:51 +0900

    [add/#20] record ui state 추가

---
+++ app/src/main/java/com/bongtu/baekseo/presentation/record/RecordContract.kt
@@ -0,0 +1,18 @@
+package com.bongtu.baekseo.presentation.record
+
+import androidx.compose.runtime.Immutable
+import com.bongtu.baekseo.core.common.state.UiState
+import com.bongtu.baekseo.core.common.type.EventType
+import com.bongtu.baekseo.data.model.DummyUser
+import com.bongtu.baekseo.data.model.record.Event
+import com.bongtu.baekseo.presentation.record.type.AttendType
+
+class RecordContract {
+    @Immutable
+    data class RecordState(
+        val recordLoadState: UiState<List<Event>> = UiState.Loading,
+        val attendType: String = AttendType.ATTEND.label,
+        val eventType: String = EventType.ALL.label,
+        val isDeleting: Boolean = false,
+    )
+}
