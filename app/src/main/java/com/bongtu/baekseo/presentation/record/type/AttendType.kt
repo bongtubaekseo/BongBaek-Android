59e0226f2ad6dad6d4c2c53ed1ff0adf8662ad96
seungjun <jjw5741@naver.com>
2025-07-06 12:10:49 +0900

    [add/#20] AttendType enum 추가

---
+++ app/src/main/java/com/bongtu/baekseo/presentation/record/type/AttendType.kt
@@ -0,0 +1,12 @@
+package com.bongtu.baekseo.presentation.record.type
+
+enum class AttendType(
+    val label: String,
+) {
+    ATTEND(
+        label = "참석"
+    ),
+    ABSENT(
+        label = "불참석"
+    );
+}
