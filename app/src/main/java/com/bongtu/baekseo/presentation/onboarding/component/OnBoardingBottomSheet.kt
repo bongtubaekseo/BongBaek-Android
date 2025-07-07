6e99ce31857379e4dbb4b9bd8eb30c1251e8da21
Hyejeong <myejeong0211@gmail.com>
2025-07-07 13:28:47 +0900

    [chore/#21] imageVector 수정

--- app/src/main/java/com/bongtu/baekseo/presentation/onboarding/component/OnBoardingBottomSheet.kt
+++ app/src/main/java/com/bongtu/baekseo/presentation/onboarding/component/OnBoardingBottomSheet.kt
@@ -27,8 +27,9 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bongtu.baekseo.R.drawable.ic_arrow_right
@@ -182,7 +183,7 @@ private fun OnBoardingAgreeItem(
 
         if (item.isArrowVisible) {
             Icon(
-                painter = painterResource(id = ic_arrow_right),
+                imageVector = ImageVector.vectorResource(id = ic_arrow_right),
                 contentDescription = null,
                 modifier = Modifier.noRippleClickable {
                     onIconClick?.invoke()
