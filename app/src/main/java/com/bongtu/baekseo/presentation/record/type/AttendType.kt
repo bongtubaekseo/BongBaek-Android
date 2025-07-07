a4e4cd77b252e3b255b68c0db3620a67af30712f
seungjun <jjw5741@naver.com>
2025-07-07 14:09:09 +0900

    [chore/#20] AttendType 줄바꿈 제거

--- app/src/main/java/com/bongtu/baekseo/presentation/record/type/AttendType.kt
+++ app/src/main/java/com/bongtu/baekseo/presentation/record/type/AttendType.kt
@@ -3,10 +3,6 @@ package com.bongtu.baekseo.presentation.record.type
 enum class AttendType(
     val label: String,
 ) {
-    ATTEND(
-        label = "참석"
-    ),
-    ABSENT(
-        label = "불참석"
-    );
+    ATTEND(label = "참석"),
+    ABSENT(label = "불참석");
 }
