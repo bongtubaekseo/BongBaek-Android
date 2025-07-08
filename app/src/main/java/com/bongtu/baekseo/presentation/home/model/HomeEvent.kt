5ccafc4791657f2b17adde6707735d9c2ea85e44
Hyejeong <myejeong0211@gmail.com>
2025-07-08 14:22:47 +0900

    [add/#30] 홈 경조사 데이터 클래스 추가

---
+++ app/src/main/java/com/bongtu/baekseo/presentation/home/model/HomeEvent.kt
@@ -0,0 +1,28 @@
+package com.bongtu.baekseo.presentation.home.model
+
+import com.bongtu.baekseo.core.common.type.EventType
+import com.bongtu.baekseo.core.common.type.RelationType
+import java.time.LocalDate
+
+data class HomeEvent(
+    val eventId: String,
+    val hostInfo: HomeHostInfo,
+    val eventInfo: HomeEventInfo,
+    val locationInfo: HomeLocationInfo,
+)
+
+data class HomeHostInfo(
+    val hostName: String,
+    val hostNickname: String,
+)
+
+data class HomeEventInfo(
+    val eventCategory: EventType,
+    val relationship: RelationType,
+    val cost: Int,
+    val eventDate: LocalDate,
+)
+
+data class HomeLocationInfo(
+    val location: String,
+)
