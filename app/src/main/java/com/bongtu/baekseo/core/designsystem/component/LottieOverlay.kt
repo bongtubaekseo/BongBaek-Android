534a8ba013aef44c0ae2d20001f2d179daae963e
Jong <jm991014@gmail.com>
2025-07-10 02:51:21 +0900

    [add/#35] Infinite, Finite 버전 구현

--- app/src/main/java/com/bongtu/baekseo/core/designsystem/component/LottieOverlay.kt
+++ app/src/main/java/com/bongtu/baekseo/core/designsystem/component/LottieOverlay.kt
@@ -4,6 +4,7 @@ import androidx.annotation.RawRes
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -11,20 +12,46 @@ import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieAnimatable
 import com.airbnb.lottie.compose.rememberLottieComposition
 import com.bongtu.baekseo.R
 import com.bongtu.baekseo.core.designsystem.theme.BongBaekTheme
 
 @Composable
-fun LottieOverlay(
+fun LottieFiniteOverlay(
     @RawRes lottieRes: Int,
     modifier: Modifier = Modifier,
-    iterations: Int = LottieConstants.IterateForever,
+    iterations: Int = 1,
+    onFinished: (Boolean) -> Unit = {},
+) {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(lottieRes))
+    val lottieAnimState = rememberLottieAnimatable()
+
+    LaunchedEffect(composition) {
+        composition?.let {
+            lottieAnimState.animate(it, iterations = iterations)
+            onFinished(true)
+        }
+    }
+
+    LottieAnimation(
+        composition = composition,
+        progress = {
+            lottieAnimState.progress
+        },
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun LottieInfiniteOverlay(
+    @RawRes lottieRes: Int,
+    modifier: Modifier = Modifier,
 ) {
     val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(lottieRes))
     val progress by animateLottieCompositionAsState(
         composition = composition,
-        iterations = iterations,
+        iterations = LottieConstants.IterateForever,
     )
 
     LottieAnimation(
@@ -44,7 +71,7 @@ private fun LottieOverlayPreview() {
             modifier = Modifier
                 .fillMaxSize(),
         ) {
-            LottieOverlay(
+            LottieInfiniteOverlay(
                 lottieRes = R.raw.lottie_find_amount,
             )
         }
