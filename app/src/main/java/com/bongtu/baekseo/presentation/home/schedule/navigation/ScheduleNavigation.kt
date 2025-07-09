b19ee67870dfcfdab29d7f7b41a306af7cd1cbd0
Hyejeong <myejeong0211@gmail.com>
2025-07-10 03:01:46 +0900

    [add/#39] 전체 일정 navigation 추가

--- app/src/main/java/com/bongtu/baekseo/presentation/home/schedule/navigation/ScheduleNavigation.kt
+++ app/src/main/java/com/bongtu/baekseo/presentation/home/schedule/navigation/ScheduleNavigation.kt
@@ -1,7 +1,22 @@
 package com.bongtu.baekseo.presentation.home.schedule.navigation
 
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
 import com.bongtu.baekseo.presentation.home.navigation.Home
+import com.bongtu.baekseo.presentation.home.navigation.HomeRoute.Schedule
+import com.bongtu.baekseo.presentation.home.schedule.ScheduleRoute
 
 fun NavController.navigateToSchedule(navOptions: NavOptions? = null) = navigate(Home, navOptions)
+
+fun NavGraphBuilder.scheduleGraph(
+    modifier: Modifier = Modifier,
+) {
+    composable<Schedule> {
+        ScheduleRoute(
+            modifier = modifier,
+        )
+    }
+}
