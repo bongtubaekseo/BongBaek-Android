9b049b837a26a625cc95e30331a20ef4d0b9f1c1
Hyejeong <myejeong0211@gmail.com>
2025-07-09 17:11:19 +0900

    [chore/#37] import 순서 수정

--- app/src/main/java/com/bongtu/baekseo/presentation/home/navigation/HomeNavigation.kt
+++ app/src/main/java/com/bongtu/baekseo/presentation/home/navigation/HomeNavigation.kt
@@ -4,10 +4,10 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
-import com.bongtu.baekseo.core.common.navigation.MainTabRoute
-import kotlinx.serialization.Serializable
 import androidx.navigation.compose.composable
+import com.bongtu.baekseo.core.common.navigation.MainTabRoute
 import com.bongtu.baekseo.presentation.home.HomeRoute
+import kotlinx.serialization.Serializable
 
 fun NavController.navigateToHome(navOptions: NavOptions? = null) = navigate(Home, navOptions)
 
