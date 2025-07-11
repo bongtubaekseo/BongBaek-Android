2933c861a245c8c2a3746cc49fffe6020a170658
seungjun <jjw5741@naver.com>
2025-07-11 11:45:13 +0900

    [ui/#43] memo content 구현

---
+++ app/src/main/java/com/bongtu/baekseo/presentation/edit/component/EditMemoContent.kt
@@ -0,0 +1,96 @@
+package com.bongtu.baekseo.presentation.edit.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import com.bongtu.baekseo.R.string.edit_memo_text_field_placeholder
+import com.bongtu.baekseo.R.string.edit_memo_title
+import com.bongtu.baekseo.core.designsystem.component.textfield.BongBaekInnerTextField
+import com.bongtu.baekseo.core.designsystem.theme.BongBaekTheme
+
+private const val MEMO_RATIO = 320 / 152f
+
+@Composable
+fun EditMemoContent(
+    text: String,
+    onTextChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isFocused by interactionSource.collectIsFocusedAsState()
+    val focusManager = LocalFocusManager.current
+
+    val bongBaekColors = BongBaekTheme.colors
+    val borderColor = if (isFocused) bongBaekColors.primaryNormal else bongBaekColors.transparent
+    val backgroundColor = if (isFocused) bongBaekColors.gray750 else bongBaekColors.gray800
+
+    Column(
+        modifier = modifier,
+    ) {
+        Text(
+            text = stringResource(edit_memo_title),
+            color = BongBaekTheme.colors.white,
+            style = BongBaekTheme.typography.titleSemiBold18,
+            modifier = Modifier
+                .padding(bottom = 10.dp),
+        )
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = backgroundColor,
+                    shape = RoundedCornerShape(10.dp),
+                )
+                .border(
+                    width = 1.dp,
+                    color = borderColor,
+                    shape = RoundedCornerShape(10.dp),
+                )
+                .aspectRatio(MEMO_RATIO),
+            contentAlignment = Alignment.TopStart,
+        ) {
+            BongBaekInnerTextField(
+                text = text,
+                onTextChange = onTextChange,
+                textColor = BongBaekTheme.colors.white,
+                placeholder = stringResource(edit_memo_text_field_placeholder),
+                placeholderColor = BongBaekTheme.colors.gray500,
+                textStyle = BongBaekTheme.typography.body2Regular16,
+                interactionSource = interactionSource,
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Done,
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        focusManager.clearFocus()
+                    },
+                ),
+                modifier = Modifier
+                    .padding(
+                        vertical = 16.dp,
+                        horizontal = 20.dp
+                    ),
+            )
+        }
+    }
+}
