f34c8130512aac04c6f82dea4c06914209020ae4
Jong <jm991014@gmail.com>
2025-07-11 15:34:00 +0900

    [add/#42] Recommend UiState 추가

---
+++ app/src/main/java/com/bongtu/baekseo/presentation/recommend/RecommendContract.kt
@@ -0,0 +1,29 @@
+package com.bongtu.baekseo.presentation.recommend
+
+import androidx.compose.runtime.Immutable
+import com.bongtu.baekseo.core.common.state.UiState
+import com.bongtu.baekseo.core.common.type.EventType
+import com.bongtu.baekseo.core.common.type.RelationType
+
+class RecommendContract {
+    @Immutable
+    data class RecommendUiState(
+        val loadState: UiState<Nothing> = UiState.Empty,
+        val pageIndex: Int = 1,
+        val name: String = "",
+        val nickname: String = "",
+        val relationType: RelationType? = null,
+        val isHighAccuracy: Boolean = false,
+        val contactFrequency: Float = 0f,
+        val meetFrequency: Float = 0f,
+        val eventType: EventType? = null,
+        val eventDate: String = "",
+        val isEventParticipated: Boolean? = null,
+        val eventLocation: Pair<Double, Double>? = null,
+        val expense: Int = 0,
+    )
+
+    sealed class RecommendSideEffect {
+        data object NavigateToResult : RecommendSideEffect()
+    }
+}
