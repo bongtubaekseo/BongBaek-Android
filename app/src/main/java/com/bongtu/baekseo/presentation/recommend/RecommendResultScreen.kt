34c24adfb4df6575f4a7e7e0e07409e9c232c1e6
Jong <jm991014@gmail.com>
2025-07-11 21:14:13 +0900

    [feat/#42] ResultScreen ViewModel/Nav 연결

--- app/src/main/java/com/bongtu/baekseo/presentation/recommend/RecommendResultScreen.kt
+++ app/src/main/java/com/bongtu/baekseo/presentation/recommend/RecommendResultScreen.kt
@@ -1,5 +1,6 @@
 package com.bongtu.baekseo.presentation.recommend
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeOut
@@ -14,6 +15,7 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -24,6 +26,9 @@ import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.flowWithLifecycle
 import com.bongtu.baekseo.R.raw.lottie_envelope
 import com.bongtu.baekseo.R.string.recommendation_result_topbar
 import com.bongtu.baekseo.core.common.type.EventType
@@ -31,13 +36,50 @@ import com.bongtu.baekseo.core.common.type.TopBarType
 import com.bongtu.baekseo.core.designsystem.component.LottieFiniteOverlay
 import com.bongtu.baekseo.core.designsystem.component.topbar.BongBaekTopBar
 import com.bongtu.baekseo.core.designsystem.theme.BongBaekTheme
+import com.bongtu.baekseo.presentation.recommend.RecommendContract.RecommendSideEffect
+import com.bongtu.baekseo.presentation.recommend.RecommendContract.RecommendSideEffect.ResultSideEffect.NavigateToFinal
 import com.bongtu.baekseo.presentation.recommend.component.RecommendExpenseCard
 import com.bongtu.baekseo.presentation.recommend.component.RecommendResultContent
+import kotlinx.coroutines.flow.filterIsInstance
 
 private const val ENVELOPE_RATIO = 320 / 291f
 
 @Composable
-fun RecommendResultScreen(
+fun RecommendResultRoute(
+    navigateToFinal: () -> Unit,
+    navigateToEdit: () -> Unit,
+    viewModel: RecommendViewModel,
+    modifier: Modifier = Modifier,
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    // TODO: EditScreen과 연결할 때 SideEffect와 연결하기
+    LaunchedEffect(viewModel.sideEffect, lifecycleOwner) {
+        viewModel.sideEffect.flowWithLifecycle(lifecycle = lifecycleOwner.lifecycle)
+            .filterIsInstance<RecommendSideEffect.ResultSideEffect>()
+            .collect { sideEffect ->
+                when (sideEffect) {
+                    is NavigateToFinal -> navigateToFinal()
+                }
+            }
+    }
+
+    BackHandler {
+        // 뒤로가기 버튼 방지 TODO: 뭔가 더 깔끔한 방법 찾아야 할 듯
+    }
+
+    RecommendResultScreen(
+        expense = uiState.expense,
+        eventType = uiState.eventType!!,
+        onConfirmClick = navigateToFinal,
+        onEditClick = navigateToEdit,
+        modifier = modifier,
+    )
+}
+
+@Composable
+private fun RecommendResultScreen(
     expense: Int,
     eventType: EventType,
     onConfirmClick: () -> Unit,
