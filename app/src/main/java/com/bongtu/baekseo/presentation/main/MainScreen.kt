bdb9d5f80481c8ca858e6d9c94bb4662fd502b2b
Hyejeong <myejeong0211@gmail.com>
2025-07-13 02:50:33 +0900

    [fix/#56] padding 수정

--- app/src/main/java/com/bongtu/baekseo/presentation/main/MainScreen.kt
+++ app/src/main/java/com/bongtu/baekseo/presentation/main/MainScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.ExitTransition
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -116,7 +117,7 @@ private fun MainNavHost(
                     }
                 )
             },
-            modifier = modifier,
+            modifier = modifier.padding(innerPadding),
         )
 
         recommendGraph(
