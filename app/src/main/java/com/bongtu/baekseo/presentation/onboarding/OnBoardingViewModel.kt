eade0e51e9abe91b3fb1a93219d80fcae036b351
Hyejeong <myejeong0211@gmail.com>
2025-07-13 02:30:23 +0900

    [chore/#51] 온보딩 update 수정

--- app/src/main/java/com/bongtu/baekseo/presentation/onboarding/OnBoardingViewModel.kt
+++ app/src/main/java/com/bongtu/baekseo/presentation/onboarding/OnBoardingViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -29,7 +30,9 @@ class OnBoardingViewModel @Inject constructor(
 
     fun loginWithKakao(token: String) {
         viewModelScope.launch {
-            _kakaoLoginState.value = SocialLoginState.Success
+            _kakaoLoginState.update {
+                SocialLoginState.Success
+            }
         }
     }
 }
