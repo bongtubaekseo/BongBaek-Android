a7659f51ae6a9451596a29e5b4022c2fb3a5329c
Jong <jm991014@gmail.com>
2025-07-12 14:13:36 +0900

    [feat/#55] EventDataSource 추상체, 구현체 작성

---
+++ app/src/main/java/com/bongtu/baekseo/data/datasource/event/EventDataSource.kt
@@ -0,0 +1,16 @@
+package com.bongtu.baekseo.data.datasource.event
+
+import com.bongtu.baekseo.core.network.model.BaseResponse
+import com.bongtu.baekseo.data.dto.event.PostEventCostRequest
+import com.bongtu.baekseo.data.dto.event.PostEventInfoRequest
+import com.bongtu.baekseo.data.dto.event.PostEventCostResponse
+
+interface EventDataSource {
+    suspend fun postEventInfo(
+        request: PostEventInfoRequest,
+    ): BaseResponse<Unit>
+
+    suspend fun postEventCost(
+        request: PostEventCostRequest,
+    ): BaseResponse<PostEventCostResponse>
+}
---
+++ app/src/main/java/com/bongtu/baekseo/data/datasourceimpl/event/EventDataSourceImpl.kt
@@ -0,0 +1,19 @@
+package com.bongtu.baekseo.data.datasourceimpl.event
+
+import com.bongtu.baekseo.data.datasource.event.EventDataSource
+import com.bongtu.baekseo.data.dto.event.PostEventCostRequest
+import com.bongtu.baekseo.data.dto.event.PostEventInfoRequest
+import com.bongtu.baekseo.data.service.event.EventService
+import javax.inject.Inject
+
+class EventDataSourceImpl @Inject constructor(
+    private val eventService: EventService,
+) : EventDataSource {
+    override suspend fun postEventInfo(
+        request: PostEventInfoRequest,
+    ) = eventService.postEventInfo(request)
+
+    override suspend fun postEventCost(
+        request: PostEventCostRequest,
+    ) = eventService.postEventCost(request)
+}
