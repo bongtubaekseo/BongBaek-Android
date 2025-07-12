60f1bd98f9d05f4f5c66ee08fffc3e1f1262e80f
seungjun <jjw5741@naver.com>
2025-07-13 03:21:08 +0900

    [move/#50] detail type 패키지 변경

--- app/src/main/java/com/bongtu/baekseo/presentation/record/type/RecordDetailDropDownTrailingType.kt
+++
@@ -1,6 +0,0 @@
-package com.bongtu.baekseo.presentation.record.type
-
-sealed interface RecordDetailDropDownTrailingType {
-    data class TrailingText(val text: String) : RecordDetailDropDownTrailingType
-    data class TrailingChip(val text: String) : RecordDetailDropDownTrailingType
-}
