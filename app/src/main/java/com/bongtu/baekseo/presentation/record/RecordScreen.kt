fa093e804b0c3b82eed2388d91da4d46ab92b337
seungjun <jjw5741@naver.com>
2025-07-13 03:23:13 +0900

    [mod/#50] Record 중첩 네비 제거

--- app/src/main/java/com/bongtu/baekseo/presentation/record/RecordScreen.kt
+++ app/src/main/java/com/bongtu/baekseo/presentation/record/RecordScreen.kt
@@ -1,41 +1,209 @@
 package com.bongtu.baekseo.presentation.record
 
-import androidx.compose.animation.EnterTransition
-import androidx.compose.animation.ExitTransition
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.bongtu.baekseo.core.common.state.UiState
 import com.bongtu.baekseo.core.designsystem.theme.BongBaekTheme
-import com.bongtu.baekseo.presentation.record.navigation.RecordRoute
-import com.bongtu.baekseo.presentation.record.navigation.recordNestedGraph
+import com.bongtu.baekseo.data.model.RecordEvent
+import com.bongtu.baekseo.presentation.record.RecordContract.RecordState
+import com.bongtu.baekseo.presentation.record.component.AttendTypeTab
+import com.bongtu.baekseo.presentation.record.component.EventCategoryBar
+import com.bongtu.baekseo.presentation.record.component.RecordEmptyContent
+import com.bongtu.baekseo.presentation.record.component.RecordListContent
+import com.bongtu.baekseo.presentation.record.component.RecordTopBar
+import com.bongtu.baekseo.presentation.record.type.AttendType
+import com.bongtu.baekseo.presentation.record.type.EventCategoryType
+import java.time.LocalDate
 
 @Composable
-fun RecordScreen(
+fun RecordRoute(
     setBottomBarVisible: (Boolean) -> Unit,
+    navigateToDetail: (String) -> Unit,
+    navigateToAdd: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: RecordViewModel = hiltViewModel(),
 ) {
-    val navController = rememberNavController()
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
-    NavHost(
-        navController = navController,
-        startDestination = RecordRoute.Default,
-        enterTransition = { EnterTransition.None },
-        exitTransition = { ExitTransition.None },
-        popEnterTransition = { EnterTransition.None },
-        popExitTransition = { ExitTransition.None },
+    if (uiState.isDeleteMode) {
+        BackHandler { viewModel.updateDeleteModeCancel() }
+    }
+
+    LaunchedEffect(uiState.isDeleteMode) { setBottomBarVisible(!uiState.isDeleteMode) }
+
+    LaunchedEffect(Unit) { viewModel.fetchRecordEvent() }
+
+    RecordScreen(
+        uiState = uiState,
+        navigateToDetail = navigateToDetail,
+        navigateToAdd = navigateToAdd,
+        onTabClick = viewModel::updateAttendType,
+        onCategoryClick = viewModel::updateEventType,
+        onEnterDeleteModeClick = viewModel::updateDeleteMode,
+        onExitDeleteModeClick = viewModel::updateDeleteModeCancel,
+        onDeleteSelectedButtonClick = viewModel::updateSelectedDeleteEventId,
+        onDeleteClick = viewModel::fetchSelectedDeleteEventIds,
+        modifier = modifier,
+    )
+}
+
+@Composable
+private fun RecordScreen(
+    uiState: RecordState,
+    navigateToDetail: (String) -> Unit,
+    navigateToAdd: () -> Unit,
+    onTabClick: (AttendType) -> Unit,
+    onCategoryClick: (EventCategoryType) -> Unit,
+    onEnterDeleteModeClick: () -> Unit,
+    onExitDeleteModeClick: () -> Unit,
+    onDeleteSelectedButtonClick: (String) -> Unit,
+    onDeleteClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val isDeleteButtonEnabled = uiState.selectedDeleteEventIds.isNotEmpty()
+
+    Column(
         modifier = modifier
-            .fillMaxSize()
-            .background(color = BongBaekTheme.colors.gray900),
+            .background(color = BongBaekTheme.colors.gray900)
     ) {
-        recordNestedGraph(
-            setBottomBarVisible = setBottomBarVisible,
-            navController = navController,
-            viewModel = viewModel,
+        RecordTopBar(
+            isDeleteMode = uiState.isDeleteMode,
+            isDeleteButtonEnabled = isDeleteButtonEnabled,
+            navigateToAdd = navigateToAdd,
+            onEnterDeleteModeClick = onEnterDeleteModeClick,
+            onExitDeleteModeClick = onExitDeleteModeClick,
+            onDeleteClick = onDeleteClick,
+        )
+
+        AttendTypeTab(
+            selectedTab = uiState.attendType,
+            onTabClick = onTabClick,
+            isEnabled = !uiState.isDeleteMode,
+        )
+
+        EventCategoryBar(
+            selectedCategory = uiState.eventCategoryType,
+            onCategoryClick = onCategoryClick,
+            isEnabled = !uiState.isDeleteMode,
+        )
+
+        when (uiState.recordLoadState) {
+            is UiState.Empty -> {
+                RecordEmptyContent()
+            }
+
+            is UiState.Failure -> {
+                // TODO: 에러 상태 화면
+            }
+
+            is UiState.Loading -> {
+                // TODO: 로딩 상태 화면
+            }
+
+            is UiState.Success -> {
+                RecordListContent(
+                    recordEventList = uiState.recordLoadState.data,
+                    isDeleteMode = uiState.isDeleteMode,
+                    selectedDeleteEventIds = uiState.selectedDeleteEventIds,
+                    onCardClick = navigateToDetail,
+                    onDeleteSelectedButtonClick = onDeleteSelectedButtonClick,
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun RecordDefaultScreenPreview() {
+    BongBaekTheme {
+        RecordScreen(
+            uiState = RecordState(
+                recordLoadState = UiState.Success(
+                    listOf(
+                        RecordEvent(
+                            eventId = "eventId",
+                            hostName = "username",
+                            hostNickName = "nickname",
+                            category = "경조사 유형",
+                            relationship = "관계",
+                            cost = 10000,
+                            eventDate = LocalDate.of(2025, 5, 4),
+                        ),
+                        RecordEvent(
+                            eventId = "eventId",
+                            hostName = "username",
+                            hostNickName = "nickname",
+                            category = "경조사 유형",
+                            relationship = "관계",
+                            cost = 10000,
+                            eventDate = LocalDate.of(2025, 5, 2),
+                        ),
+                        RecordEvent(
+                            eventId = "eventId",
+                            hostName = "username",
+                            hostNickName = "nickname",
+                            category = "경조사 유형",
+                            relationship = "관계",
+                            cost = 10000,
+                            eventDate = LocalDate.of(2025, 4, 10),
+                        ),
+                        RecordEvent(
+                            eventId = "eventId",
+                            hostName = "username",
+                            hostNickName = "nickname",
+                            category = "경조사 유형",
+                            relationship = "관계",
+                            cost = 10000,
+                            eventDate = LocalDate.of(2025, 2, 23),
+                        ),
+                        RecordEvent(
+                            eventId = "eventId",
+                            hostName = "username",
+                            hostNickName = "nickname",
+                            category = "경조사 유형",
+                            relationship = "관계",
+                            cost = 10000,
+                            eventDate = LocalDate.of(2024, 6, 8),
+                        ),
+                        RecordEvent(
+                            eventId = "eventId",
+                            hostName = "username",
+                            hostNickName = "nickname",
+                            category = "경조사 유형",
+                            relationship = "관계",
+                            cost = 10000,
+                            eventDate = LocalDate.of(2024, 5, 24),
+                        ),
+                        RecordEvent(
+                            eventId = "eventId",
+                            hostName = "username",
+                            hostNickName = "nickname",
+                            category = "경조사 유형",
+                            relationship = "관계",
+                            cost = 10000,
+                            eventDate = LocalDate.of(2023, 2, 4),
+                        ),
+                    ),
+                ),
+            ),
+            onTabClick = {},
+            onCategoryClick = {},
+            onEnterDeleteModeClick = {},
+            onExitDeleteModeClick = {},
+            onDeleteSelectedButtonClick = {},
+            onDeleteClick = {},
+            navigateToDetail = {},
+            navigateToAdd = {},
+            modifier = Modifier,
         )
     }
 }
--- app/src/main/java/com/bongtu/baekseo/presentation/record/navigation/RecordNavigation.kt
+++ app/src/main/java/com/bongtu/baekseo/presentation/record/navigation/RecordNavigation.kt
@@ -3,43 +3,29 @@ package com.bongtu.baekseo.presentation.record.navigation
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavHostController
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import com.bongtu.baekseo.core.common.navigation.MainTabRoute
-import com.bongtu.baekseo.presentation.record.RecordDefaultRoute
-import com.bongtu.baekseo.presentation.record.RecordScreen
-import com.bongtu.baekseo.presentation.record.RecordViewModel
-import com.bongtu.baekseo.presentation.record.navigation.RecordRoute.Default
+import com.bongtu.baekseo.presentation.edit.type.EditType
+import com.bongtu.baekseo.presentation.record.RecordRoute
 import kotlinx.serialization.Serializable
 
 fun NavController.navigateToRecord(navOptions: NavOptions? = null) = navigate(Record, navOptions)
 
 fun NavGraphBuilder.recordGraph(
     setBottomBarVisible: (Boolean) -> Unit,
+    navigateToAdd: (EditType) -> Unit,
+    navigateToDetail: (String) -> Unit,
     modifier: Modifier = Modifier,
 ) {
     composable<Record> {
-        RecordScreen(
+        RecordRoute(
             setBottomBarVisible = setBottomBarVisible,
+            navigateToDetail = navigateToDetail,
+            navigateToAdd = { navigateToAdd(EditType.ADD) },
             modifier = modifier,
         )
     }
-}
-
-fun NavGraphBuilder.recordNestedGraph(
-    setBottomBarVisible: (Boolean) -> Unit,
-    navController: NavHostController,
-    viewModel: RecordViewModel,
-) {
-    composable<Default> {
-        RecordDefaultRoute(
-            setBottomBarVisible = setBottomBarVisible,
-            viewModel = viewModel,
-        )
-    }
-
-    // TODO: 수정 페이지
 }
 
 @Serializable
