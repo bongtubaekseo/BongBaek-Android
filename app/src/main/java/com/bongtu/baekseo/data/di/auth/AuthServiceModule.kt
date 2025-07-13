a71d05600ade06ec17bdec02a078931ae88a5b58
Hyejeong <myejeong0211@gmail.com>
2025-07-13 20:51:15 +0900

    [add/#63] auth service module 추가

---
+++ app/src/main/java/com/bongtu/baekseo/data/di/auth/AuthServiceModule.kt
@@ -0,0 +1,18 @@
+package com.bongtu.baekseo.data.di.auth
+
+import com.bongtu.baekseo.data.service.auth.AuthService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AuthServiceModule {
+    @Provides
+    @Singleton
+    fun provideAuthService(retrofit: Retrofit): AuthService = retrofit.create()
+}
