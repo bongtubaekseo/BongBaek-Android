93b18ab20cea55b65cfc04f6e2688ef426733869
Jong <jm991014@gmail.com>
2025-07-13 21:15:49 +0900

    [feat/#58] Place model 및 mapper 추가

---
+++ app/src/main/java/com/bongtu/baekseo/data/model/map/Place.kt
@@ -0,0 +1,10 @@
+package com.bongtu.baekseo.data.model.map
+
+data class Place (
+    val id: String,
+    val name: String,
+    val address: String,
+    val roadAddress: String,
+    val latitude: Double,
+    val longitude: Double,
+)
