e06074b0c0ce0fb63af40ded48284555c1c5c833
Jong <jm991014@gmail.com>
2025-07-14 19:29:41 +0900

    [feat/#65] 금액추천 반환, 경조사 등록 API 연결

--- app/src/main/java/com/bongtu/baekseo/presentation/recommend/RecommendViewModel.kt
+++ app/src/main/java/com/bongtu/baekseo/presentation/recommend/RecommendViewModel.kt
@@ -4,6 +4,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.bongtu.baekseo.core.common.type.EventType
 import com.bongtu.baekseo.core.common.type.RelationType
+import com.bongtu.baekseo.data.model.event.Event
+import com.bongtu.baekseo.data.model.event.HighAccuracy
+import com.bongtu.baekseo.data.model.event.Host
+import com.bongtu.baekseo.data.model.event.Location
+import com.bongtu.baekseo.data.repository.event.EventRepository
 import com.bongtu.baekseo.data.repository.map.KakaoMapRepository
 import com.bongtu.baekseo.presentation.recommend.RecommendContract.RecommendSideEffect
 import com.bongtu.baekseo.presentation.recommend.RecommendContract.RecommendSideEffect.MainSideEffect
@@ -16,12 +21,14 @@ import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
+import kotlin.math.roundToInt
 
 @HiltViewModel
 class RecommendViewModel @Inject constructor(
-    // TODO: Repository 주입
     private val kakaoMapRepository: KakaoMapRepository,
+    private val eventRepository: EventRepository,
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(RecommendUiState())
     val uiState = _uiState.asStateFlow()
@@ -62,7 +69,8 @@ class RecommendViewModel @Inject constructor(
     }
 
     fun updateEventDate(newEventDate: String) = _uiState.update {
-        it.copy(eventDate = newEventDate)
+        val formattedDate = "${newEventDate.substring(0, 2)}/${newEventDate.substring(2, 4)}/${newEventDate.substring(4)}"
+        it.copy(eventDate = formattedDate)
     }
 
     fun updateIsEventParticipated(newIsEventParticipated: Boolean) = _uiState.update {
@@ -92,11 +100,42 @@ class RecommendViewModel @Inject constructor(
     }
 
     fun fetchExpense() = viewModelScope.launch {
-        // TODO: Repository 연결(경조사비 추천 GET 요청)
-        _sideEffect.emit(MainSideEffect.NavigateToResult)
+        with(uiState.value) {
+            eventRepository.postEventCost(
+                event = Event(
+                    eventType = eventType!!.label,
+                    relationType = relationType!!.label,
+                    cost = expense,
+                    isEventParticipated = isEventParticipated!!,
+                    eventDate = eventDate,
+                    note = "",
+                ),
+                location = Location(
+                    location = location,
+                    address = address,
+                    latitude = latitude,
+                    longitude = longitude,
+                ),
+                highAccuracy = HighAccuracy(
+                    contactFrequency = if (isHighAccuracy) contactFrequency.roundToInt() else DEFAULT_WEIGHT,
+                    meetFrequency = if (isHighAccuracy) meetFrequency.roundToInt() else DEFAULT_WEIGHT,
+                )
+            ).onSuccess {
+                _uiState.update {
+                    it.copy(
+                        expense = it.expense,
+                    )
+                }
+                Timber.d("fetchExpense: $it")
+                _sideEffect.emit(MainSideEffect.NavigateToResult)
+            }.onFailure {
+                // TODO: 실패 처리
+            }
+        }
     }
 
     fun searchPlaces(query: String) = viewModelScope.launch {
+        // TODO: 여유되면 Debounce 추가
         kakaoMapRepository.searchPlaces(query).onSuccess {
             _uiState.update {
                 it.copy(
@@ -107,7 +146,41 @@ class RecommendViewModel @Inject constructor(
     }
 
     fun saveEventInformation() = viewModelScope.launch {
-        // TODO: Repository 연결(경조사 정보 POST 요청)
-        _sideEffect.emit(ResultSideEffect.NavigateToFinal)
+        with(uiState.value) {
+            // TODO: 반복되는 부분은 추후 리팩토링해서 묶을 수 있게끔 설정
+            // TODO: null-assertion vs elvis 멘토링 이후에 리팩
+            eventRepository.postEventInfo(
+                host = Host(
+                    name = name,
+                    nickname = nickname,
+                ),
+                event = Event(
+                    eventType = eventType!!.label,
+                    relationType = relationType!!.label,
+                    cost = expense,
+                    isEventParticipated = isEventParticipated!!,
+                    eventDate = eventDate.replace("/", "-"),
+                    note = "",
+                ),
+                location = Location(
+                    location = location,
+                    address = address,
+                    latitude = latitude,
+                    longitude = longitude,
+                ),
+                highAccuracy = HighAccuracy(
+                    contactFrequency = if (isHighAccuracy) contactFrequency.roundToInt() else DEFAULT_WEIGHT,
+                    meetFrequency = if (isHighAccuracy) meetFrequency.roundToInt() else DEFAULT_WEIGHT,
+                )
+            )
+        }.onSuccess {
+            _sideEffect.emit(ResultSideEffect.NavigateToFinal)
+        }.onFailure {
+            // TODO: 실패 처리
+        }
+    }
+
+    companion object {
+        private const val DEFAULT_WEIGHT = 3
     }
 }
