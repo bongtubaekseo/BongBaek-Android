83e693a7104e9959e5c0a9b16be72c1d8f04e1ac
Hyejeong <myejeong0211@gmail.com>
2025-07-14 22:49:06 +0900

    [fix/#63] 유저 이름 저장 수정

--- app/src/main/java/com/bongtu/baekseo/domain/usecase/SetKakaoLoginUseCase.kt
+++ app/src/main/java/com/bongtu/baekseo/domain/usecase/SetKakaoLoginUseCase.kt
@@ -1,6 +1,7 @@
 package com.bongtu.baekseo.domain.usecase
 
 import com.bongtu.baekseo.core.local.datastore.TokenDataStore
+import com.bongtu.baekseo.core.local.datastore.UsernameDataStore
 import com.bongtu.baekseo.data.model.auth.KakaoLogin
 import com.bongtu.baekseo.data.repository.auth.AuthRepository
 import javax.inject.Inject
@@ -8,6 +9,7 @@ import javax.inject.Inject
 class SetKakaoLoginUseCase @Inject constructor(
     private val authRepository: AuthRepository,
     private val tokenDataStore: TokenDataStore,
+    private val usernameDataStore: UsernameDataStore,
 ) {
     suspend operator fun invoke(kakaoToken: String): Result<KakaoLogin> {
         return authRepository.postKakaoLogin(kakaoToken)
@@ -16,6 +18,7 @@ class SetKakaoLoginUseCase @Inject constructor(
                     accessToken = response.accessToken,
                     refreshToken = response.refreshToken,
                 )
+                usernameDataStore.setUsername(response.name)
             }
     }
 }
