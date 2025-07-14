5bb31fe58d238ddbf0cbe09006bf6784364c8b1a
Hyejeong <myejeong0211@gmail.com>
2025-07-14 22:04:11 +0900

    [del/#63] 불필요 주석 삭제

--- app/src/main/java/com/bongtu/baekseo/core/util/DateFormat.kt
+++ app/src/main/java/com/bongtu/baekseo/core/util/DateFormat.kt
@@ -1,7 +1,7 @@
 package com.bongtu.baekseo.core.util
 
 fun String.toFormattedDate(): String {
-    if (this.length != 8) return "" // 잘못된 길이일 경우 빈 문자열 반환
+    if (this.length != 8) return ""
     val month = this.substring(0, 2)
     val day = this.substring(2, 4)
     val year = this.substring(4, 8)
