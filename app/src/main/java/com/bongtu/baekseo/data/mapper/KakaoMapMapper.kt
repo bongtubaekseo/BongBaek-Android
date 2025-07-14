a6f8daa58e260d79b30eb61f606ee1f900da6338
Jong <jm991014@gmail.com>
2025-07-14 19:19:29 +0900

    [fix/#58] Mapper 함수 간소화

--- app/src/main/java/com/bongtu/baekseo/data/mapper/KakaoMapMapper.kt
+++ app/src/main/java/com/bongtu/baekseo/data/mapper/KakaoMapMapper.kt
@@ -3,13 +3,11 @@ package com.bongtu.baekseo.data.mapper
 import com.bongtu.baekseo.data.dto.map.KakaoMapResponse
 import com.bongtu.baekseo.data.model.map.Place
 
-fun KakaoMapResponse.PlaceDocument.toModel(): Place {
-    return Place(
-        id = id,
-        name = placeName,
-        address = addressName,
-        roadAddress = roadAddress,
-        latitude = y.toDoubleOrNull() ?: 0.0,
-        longitude = x.toDoubleOrNull() ?: 0.0,
-    )
-}
+fun KakaoMapResponse.PlaceDocument.toModel() = Place(
+    id = id,
+    name = placeName,
+    address = addressName,
+    roadAddress = roadAddress,
+    latitude = y.toDoubleOrNull() ?: 0.0,
+    longitude = x.toDoubleOrNull() ?: 0.0,
+)
