d4fa165781c9ea6c56132db1fb70129e291357a8
seungjun <jjw5741@naver.com>
2025-07-16 00:04:33 +0900

    [add/#75] uiState 정의

---
+++ app/src/main/java/com/bongtu/baekseo/presentation/edit/EditContract.kt
@@ -0,0 +1,21 @@
+package com.bongtu.baekseo.presentation.edit
+
+import androidx.compose.runtime.Immutable
+import com.bongtu.baekseo.core.common.state.UiState
+import com.bongtu.baekseo.data.dto.event.FetchHomeEventsResponse.LocationInfo
+
+class EditContract {
+    @Immutable
+    data class EditUiState(
+        val submitState: UiState<Unit> = UiState.Empty,
+        val hostName: String = "",
+        val hostNickname: String = "",
+        val eventCategory: String = "",
+        val relationship: String = "",
+        val cost: String = "",
+        val isEventParticipated: Boolean = false,
+        val eventDate: String = "",
+        val note: String = "",
+        val locationInfo: LocationInfo? = null,
+    )
+}
--- app/src/main/java/com/bongtu/baekseo/presentation/edit/EditViewModel.kt
+++ app/src/main/java/com/bongtu/baekseo/presentation/edit/EditViewModel.kt
@@ -1,12 +1,81 @@
 package com.bongtu.baekseo.presentation.edit
 
 import androidx.lifecycle.ViewModel
+import com.bongtu.baekseo.data.dto.event.FetchHomeEventsResponse.LocationInfo
+import com.bongtu.baekseo.presentation.edit.EditContract.EditUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class EditViewModel @Inject constructor(
     // TODO: repository 추가 예정
 ) : ViewModel() {
+    private val _uiState = MutableStateFlow(EditUiState())
+    val uiState = _uiState.asStateFlow()
 
+    fun updateHostName(newHostName: String) =
+        _uiState.update { currentState ->
+            currentState.copy(
+                hostName = newHostName,
+            )
+        }
+
+    fun updateHostNickname(newHostNickname: String) =
+        _uiState.update { currentState ->
+            currentState.copy(
+                hostNickname = newHostNickname,
+            )
+        }
+
+    fun updateEventCategory(newEventCategory: String) =
+        _uiState.update { currentState ->
+            currentState.copy(
+                eventCategory = newEventCategory,
+            )
+        }
+
+    fun updateRelationship(newRelationship: String) =
+        _uiState.update { currentState ->
+            currentState.copy(
+                relationship = newRelationship,
+            )
+        }
+
+    fun updateCost(newCost: String) =
+        _uiState.update { currentState ->
+            currentState.copy(
+                cost = newCost,
+            )
+        }
+
+    fun updateIsEventParticipated(newIsEventParticipated: Boolean) =
+        _uiState.update { currentState ->
+            currentState.copy(
+                isEventParticipated = newIsEventParticipated,
+            )
+        }
+
+    fun updateEventDate(newEventDate: String) =
+        _uiState.update { currentState ->
+            currentState.copy(
+                eventDate = newEventDate,
+            )
+        }
+
+    fun updateNote(newNote: String) =
+        _uiState.update { currentState ->
+            currentState.copy(
+                note = newNote,
+            )
+        }
+
+    fun updateLocationInfo(newLocationInfo: LocationInfo?) =
+        _uiState.update { currentState ->
+            currentState.copy(
+                locationInfo = newLocationInfo,
+            )
+        }
 }
