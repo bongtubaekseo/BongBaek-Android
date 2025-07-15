cc0d69b3272d88358e46d9631a3b9b95ebb5e987
seungjun <jjw5741@naver.com>
2025-07-15 12:17:51 +0900

    [move/#62] AttendType core 패키지 이동

--- app/src/main/java/com/bongtu/baekseo/presentation/record/component/AttendTypeTab.kt
+++ app/src/main/java/com/bongtu/baekseo/presentation/record/component/AttendTypeTab.kt
@@ -22,7 +22,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bongtu.baekseo.core.designsystem.theme.BongBaekTheme
 import com.bongtu.baekseo.core.util.noRippleClickable
-import com.bongtu.baekseo.presentation.record.type.AttendType
+import com.bongtu.baekseo.core.common.type.AttendType
 
 @Immutable
 private data class TabStyle(
