3184bebc3fbe99b8cdde7c86fdf678251c35e063
Jong <jm991014@gmail.com>
2025-07-15 22:46:47 +0900

    [mod/#73] 중첩 네비 제거 후 RecommendGraph로 이동

--- app/src/main/java/com/bongtu/baekseo/presentation/recommend/navigation/RecommendNavigation.kt
+++ app/src/main/java/com/bongtu/baekseo/presentation/recommend/navigation/RecommendNavigation.kt
@@ -6,103 +6,91 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
-import androidx.navigation.navOptions
+import androidx.navigation.compose.navigation
 import com.bongtu.baekseo.core.common.navigation.MainTabRoute
-import com.bongtu.baekseo.presentation.edit.type.EditType
+import com.bongtu.baekseo.core.common.navigation.Route
+import com.bongtu.baekseo.core.util.sharedViewModel
 import com.bongtu.baekseo.presentation.recommend.RecommendFinalRoute
 import com.bongtu.baekseo.presentation.recommend.RecommendIntroScreen
 import com.bongtu.baekseo.presentation.recommend.RecommendMainRoute
 import com.bongtu.baekseo.presentation.recommend.RecommendResultRoute
-import com.bongtu.baekseo.presentation.recommend.RecommendRoute
 import com.bongtu.baekseo.presentation.recommend.RecommendViewModel
 import kotlinx.serialization.Serializable
 
 fun NavController.navigateToRecommend(navOptions: NavOptions? = null) =
     navigate(Recommend, navOptions)
 
-fun NavGraphBuilder.recommendGraph(
-    navigateToUp: () -> Unit,
-    navigateToHome: () -> Unit,
-    navigateToRecord: () -> Unit,
-    navigateToEdit: () -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    composable<Recommend> {
-        RecommendRoute(
-            navigateToUp = navigateToUp,
-            navigateToEdit = navigateToEdit,
-            navigateToHome = navigateToHome,
-            navigateToRecord = navigateToRecord,
-            modifier = modifier,
-        )
-    }
-}
+fun NavController.navigateToRecommendMain(navOptions: NavOptions? = null) =
+    navigate(RecommendMain, navOptions)
 
-fun NavGraphBuilder.nestedRecommendGraph(
+fun NavController.navigateToRecommendResult(navOptions: NavOptions? = null) =
+    navigate(RecommendResult, navOptions)
+
+fun NavHostController.navigateToRecommendFinal(navOptions: NavOptions? = null) =
+    navigate(RecommendFinal, navOptions)
+
+fun NavGraphBuilder.recommendGraph(
     navController: NavHostController,
     navigateToUp: () -> Unit,
-    navigateToEdit: () -> Unit,
     navigateToHome: () -> Unit,
     navigateToRecord: () -> Unit,
-    viewModel: RecommendViewModel,
+    navigateToEdit: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    composable<RecommendRoute.Intro> {
-        RecommendIntroScreen(
-            navigateToUp = navigateToUp,
-            navigateToNext = {
-                navController.navigate(RecommendRoute.Main)
-            },
-            modifier = modifier,
-        )
-    }
+    navigation<Recommend>(
+        startDestination = RecommendIntro,
+    ) {
+        composable<RecommendIntro> {
+            RecommendIntroScreen(
+                navigateToUp = navigateToUp,
+                navigateToNext = { navController.navigateToRecommendMain() },
+                modifier = modifier,
+            )
+        }
 
-    composable<RecommendRoute.Main> {
-        RecommendMainRoute(
-            navigateToUp = navController::navigateUp,
-            navigateToResult = {
-                navController.navigate(
-                    route = RecommendRoute.Result,
-                    navOptions = navOptions {
-                        popUpTo(RecommendRoute.Intro) {
-                            inclusive = true
-                        }
-                        launchSingleTop = true
-                    }
-                )
-            },
-            viewModel = viewModel,
-            modifier = modifier,
-        )
-    }
+        composable<RecommendMain> { backStackEntry ->
+            val viewModel = backStackEntry.sharedViewModel<RecommendViewModel>(navController)
 
-    composable<RecommendRoute.Result> {
-        RecommendResultRoute(
-            navigateToFinal = {
-                navController.navigate(
-                    route = RecommendRoute.Final,
-                    navOptions = navOptions {
-                        popUpTo(RecommendRoute.Final) {
-                            inclusive = true
-                        }
-                        launchSingleTop = true
-                    }
-                )
-            },
-            navigateToEdit = navigateToEdit,
-            viewModel = viewModel,
-            modifier = modifier,
-        )
-    }
+            RecommendMainRoute(
+                navigateToUp = navController::navigateUp,
+                navigateToResult = { navController.navigateToRecommendResult() },
+                viewModel = viewModel,
+                modifier = modifier,
+            )
+        }
 
-    composable<RecommendRoute.Final> {
-        RecommendFinalRoute(
-            navigateToHome = navigateToHome,
-            navigateToRecord = navigateToRecord,
-            modifier = modifier,
-        )
+        composable<RecommendResult> { backStackEntry ->
+            val viewModel = backStackEntry.sharedViewModel<RecommendViewModel>(navController)
+
+            RecommendResultRoute(
+                navigateToFinal = { navController.navigateToRecommendFinal() },
+                navigateToEdit = navigateToEdit,
+                viewModel = viewModel,
+                modifier = modifier,
+            )
+        }
+
+        composable<RecommendFinal> {
+            RecommendFinalRoute(
+                navigateToHome = navigateToHome,
+                navigateToRecord = navigateToRecord,
+                modifier = modifier,
+            )
+        }
     }
 }
 
 @Serializable
 data object Recommend : MainTabRoute
+
+@Serializable
+data object RecommendIntro : Route
+
+@Serializable
+data object RecommendMain : Route
+
+@Serializable
+data object RecommendResult : Route
+
+@Serializable
+data object RecommendFinal : Route
