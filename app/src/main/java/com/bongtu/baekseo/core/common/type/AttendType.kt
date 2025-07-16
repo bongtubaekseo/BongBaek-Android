20a0af91f144828017a1c68e6b3ad98d2fb31c5b
Hyejeong <myejeong0211@gmail.com>
2025-07-16 18:46:56 +0900

    [chore/#88] attendType 수정

--- app/src/main/java/com/bongtu/baekseo/core/common/type/AttendType.kt
+++ app/src/main/java/com/bongtu/baekseo/core/common/type/AttendType.kt
@@ -2,7 +2,14 @@ package com.bongtu.baekseo.core.common.type
 
 enum class AttendType(
     val label: String,
+    val isAttended: Boolean,
 ) {
-    ATTEND(label = "참석"),
-    ABSENT(label = "불참석");
+    ATTEND(
+        label = "참석",
+        isAttended = true,
+    ),
+    ABSENT(
+        label = "불참석",
+        isAttended = false,
+    );
 }
