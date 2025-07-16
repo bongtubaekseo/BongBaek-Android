d0e2c1c5bb6db962da500e96e6b2cb45b305e33b
seungjun <jjw5741@naver.com>
2025-07-16 15:10:44 +0900

    [chore/#75] 에러메시지 상수화

--- app/src/main/java/com/bongtu/baekseo/core/designsystem/component/textfield/TextFieldValidateResult.kt
+++ app/src/main/java/com/bongtu/baekseo/core/designsystem/component/textfield/TextFieldValidateResult.kt
@@ -24,22 +24,27 @@ sealed class TextFieldValidateResult {
 
     companion object {
         private val SPECIAL_CHAR_REGEX = Regex("[!@#\$%^&*(),.?\":{}|<>]")
+        private const val ERROR_LENGTH_MESSAGE = "2자 이상 10자 이내로 입력해주세요"
+        private const val ERROR_SPECIAL_CHARACTER_MESSAGE = "특수문자는 사용할 수 없어요"
+        private const val ERROR_ZERO_COST_MESSAGE = "1원 이상 적어주세요"
 
         fun validate(input: String): TextFieldValidateResult {
             return when {
                 input.length < 2 || input.length > 10 ->
-                    Error("2자 이상 10자 이내로 입력해주세요.")
+                    Error(ERROR_LENGTH_MESSAGE)
+
                 SPECIAL_CHAR_REGEX.containsMatchIn(input) ->
-                    Error("특수문자는 사용할 수 없어요.")
+                    Error(ERROR_SPECIAL_CHARACTER_MESSAGE)
+
                 else -> Default
             }
         }
 
-        fun validateCost(cost: String) : TextFieldValidateResult {
+        fun validateCost(cost: String): TextFieldValidateResult {
             val number = cost.toIntOrNull() ?: 0
 
             return when {
-                number < 1 -> Error("1원 이상 적어주세요")
+                number < 1 -> Error(ERROR_ZERO_COST_MESSAGE)
                 else -> Default
             }
         }
