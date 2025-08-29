7144d268536235743bf4b0b7ad27605217a28b6d
Hyejeong <myejeong0211@gmail.com>
2025-08-29 21:30:50 +0900

    [chore/#140] 기타 포커스 수정

---
+++ app/src/main/java/com/bongtu/baekseo/presentation/withdraw/WithdrawScreen.kt
@@ -0,0 +1,176 @@
+package com.bongtu.baekseo.presentation.withdraw
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.bongtu.baekseo.R
+import com.bongtu.baekseo.R.drawable.ic_arrow_back
+import com.bongtu.baekseo.R.string.withdraw_button_title
+import com.bongtu.baekseo.R.string.withdraw_topbar
+import com.bongtu.baekseo.core.common.type.ButtonType
+import com.bongtu.baekseo.core.common.type.TopBarType
+import com.bongtu.baekseo.core.common.type.WithdrawType
+import com.bongtu.baekseo.core.designsystem.component.button.BongBaekButton
+import com.bongtu.baekseo.core.designsystem.component.topbar.BongBaekTopBar
+import com.bongtu.baekseo.core.designsystem.theme.BongBaekTheme
+import com.bongtu.baekseo.core.util.noRippleClickable
+import com.bongtu.baekseo.presentation.withdraw.WithdrawContract.WithdrawUiState
+import com.bongtu.baekseo.presentation.withdraw.component.WithdrawDialog
+import com.bongtu.baekseo.presentation.withdraw.component.WithdrawReasonSelector
+
+@Composable
+fun WithdrawRoute(
+    modifier: Modifier = Modifier,
+    viewModel: WithdrawViewModel = hiltViewModel()
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val buttonEnabled = remember(uiState.reasonType, uiState.etcReason) {
+        viewModel.updateButtonState()
+    }
+
+    WithdrawScreen(
+        uiState = uiState,
+        buttonEnabled = buttonEnabled,
+        navigateToUp = {},
+        onReasonSelect = viewModel::updateReasonType,
+        modifier = modifier,
+        onEtcValueChange = viewModel::updateEtcReason,
+    )
+}
+
+@Composable
+private fun WithdrawScreen(
+    uiState: WithdrawUiState,
+    buttonEnabled: Boolean,
+    navigateToUp: () -> Unit,
+    onReasonSelect: (WithdrawType) -> Unit,
+    modifier: Modifier = Modifier,
+    onEtcValueChange: (String) -> Unit = {},
+) {
+    var isWithdrawDialogVisible by remember { mutableStateOf(false) }
+    val focusManager = LocalFocusManager.current
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = BongBaekTheme.colors.gray900)
+            .pointerInput(Unit) {
+                detectTapGestures(onTap = { focusManager.clearFocus() })
+            },
+    ) {
+        BongBaekTopBar(
+            title = stringResource(withdraw_topbar),
+            topBarType = TopBarType.LEADING_ICON,
+            leadingIcon = {
+                Icon(
+                    imageVector = ImageVector.vectorResource(ic_arrow_back),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(12.dp)
+                        .noRippleClickable(navigateToUp),
+                    tint = BongBaekTheme.colors.white,
+                )
+            },
+            modifier = Modifier
+                .statusBarsPadding(),
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 20.dp),
+            verticalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Column {
+                Spacer(modifier = Modifier.height(18.dp))
+
+                Text(
+                    text = stringResource(id = R.string.withdraw_title),
+                    style = BongBaekTheme.typography.headBold24,
+                    color = BongBaekTheme.colors.gray100,
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Text(
+                    text = stringResource(id = R.string.withdraw_information),
+                    style = BongBaekTheme.typography.body2Regular14,
+                    color = BongBaekTheme.colors.gray400,
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                WithdrawReasonSelector(
+                    value = uiState.etcReason,
+                    onValueChange = onEtcValueChange,
+                    selectedReason = uiState.reasonType,
+                    onReasonSelect = onReasonSelect,
+                )
+            }
+
+            BongBaekButton(
+                title = stringResource(id = withdraw_button_title),
+                onClick = {
+                    isWithdrawDialogVisible = true
+                },
+                buttonType = ButtonType.PRIMARY,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        bottom = 36.dp,
+                    ),
+                enabled = buttonEnabled,
+            )
+        }
+
+        if (isWithdrawDialogVisible) {
+            WithdrawDialog(
+                onDismissRequest = {
+                    isWithdrawDialogVisible = false
+                },
+                onConfirmClick = {
+                    isWithdrawDialogVisible = false
+                },
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun WithdrawScreenPreview() {
+    BongBaekTheme {
+        WithdrawScreen(
+            uiState = WithdrawUiState(
+                reasonType = WithdrawType.UNCOMFORTABLE,
+            ),
+            buttonEnabled = true,
+            navigateToUp = {},
+            onReasonSelect = {},
+        )
+    }
+}
