dc0ebf5e6d0417d611c1d2e3c1ba35809feca102
Hyejeong <myejeong0211@gmail.com>
2025-08-29 22:29:47 +0900

    [chore/#140] 탈퇴 dialog 수정

--- app/src/main/java/com/bongtu/baekseo/presentation/withdraw/component/WithdrawDialog.kt
+++ app/src/main/java/com/bongtu/baekseo/presentation/withdraw/component/WithdrawDialog.kt
@@ -10,11 +10,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,9 +41,11 @@ fun WithdrawDialog(
             usePlatformDefaultWidth = false,
         ),
     ) {
-        Card(
-            modifier = Modifier.padding(horizontal = 20.dp),
-            shape = RoundedCornerShape(10.dp),
+        Box(
+            modifier = Modifier
+                .padding(horizontal = 20.dp)
+                .clip(RoundedCornerShape(10.dp))
+                .background(BongBaekTheme.colors.gray750),
         ) {
             Column(
                 modifier = modifier
