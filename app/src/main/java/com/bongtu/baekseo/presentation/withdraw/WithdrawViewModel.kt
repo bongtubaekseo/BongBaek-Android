5d5c14ec0b1b3a21a52408590118f99729dfe332
Hyejeong <myejeong0211@gmail.com>
2025-08-29 21:31:13 +0900

    [add/#140] 탈퇴 ViewModel 추가

---
+++ app/src/main/java/com/bongtu/baekseo/presentation/withdraw/WithdrawViewModel.kt
@@ -0,0 +1,31 @@
+package com.bongtu.baekseo.presentation.withdraw
+
+import androidx.lifecycle.ViewModel
+import com.bongtu.baekseo.core.common.type.WithdrawType
+import com.bongtu.baekseo.presentation.withdraw.WithdrawContract.WithdrawUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class WithdrawViewModel @Inject constructor(
+
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(WithdrawUiState())
+    val uiState = _uiState.asStateFlow()
+
+    fun updateReasonType(newReasonType: WithdrawType) = _uiState.update {
+        it.copy(reasonType = newReasonType)
+    }
+
+    fun updateButtonState(): Boolean =
+        with(uiState.value) {
+            return reasonType != null && (reasonType != WithdrawType.ETC || etcReason.isNotBlank())
+        }
+
+    fun updateEtcReason(newEtcReason: String) = _uiState.update {
+        it.copy(etcReason = newEtcReason)
+    }
+}
