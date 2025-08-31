45cb3884fcb7675bcaf7bf68afb1f093ecae2c41
seungjun <jjw5741@naver.com>
2025-08-31 15:44:46 +0900

    [chore/#139] TODO 탙퇴하기 뷰 이동 추가

--- app/src/main/java/com/bongtu/baekseo/presentation/main/MainScreen.kt
+++ app/src/main/java/com/bongtu/baekseo/presentation/main/MainScreen.kt
@@ -205,7 +205,7 @@ private fun MainNavHost(
         myPageGraph(
             navController = navigator.navController,
             navigateUp = navigator::navigateUp,
-            navigateToWithDraw = {},
+            navigateToWithDraw = { /* TODO: 탈퇴하기 뷰 이동 */ },
             modifier = modifier,
         )
     }
