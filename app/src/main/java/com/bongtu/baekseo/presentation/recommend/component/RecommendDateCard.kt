418baf63b80651ba71df4ad37fcab23347a9cd7e
seungjun <jjw5741@naver.com>
2025-09-01 23:44:08 +0900

    [chore/#145] 필요없는 import 제거

--- app/src/main/java/com/bongtu/baekseo/presentation/recommend/component/RecommendDateCard.kt
+++ app/src/main/java/com/bongtu/baekseo/presentation/recommend/component/RecommendDateCard.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
