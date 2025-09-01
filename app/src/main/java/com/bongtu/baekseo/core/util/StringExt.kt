4e56c4999e49d955f8d19343b2bd0770616ac923
seungjun <jjw5741@naver.com>
2025-09-01 23:14:56 +0900

    [del/#145] 필요없는 문자열 확장함수 제거

--- app/src/main/java/com/bongtu/baekseo/core/util/StringExt.kt
+++ app/src/main/java/com/bongtu/baekseo/core/util/StringExt.kt
@@ -6,17 +6,6 @@ import java.time.format.TextStyle
 import java.util.Locale
 
 /**
- * mmddyyyy -> yyyy-mm-dd
- */
-fun String.toFormattedDate(): String {
-    if (this.length != 8) return ""
-    val month = this.substring(0, 2)
-    val day = this.substring(2, 4)
-    val year = this.substring(4, 8)
-    return "$year-$month-$day"
-}
-
-/**
  * yyyy-mm-dd -> yyyy. mm. dd (day)
  */
 fun String.toFormattedDateWithDay(): String = try {
@@ -52,14 +41,6 @@ fun String.toFormattedYearWithMonthPair(): Pair<Int, Int> = try {
     val localDate = LocalDate.parse(this, DateTimeFormatter.ISO_DATE)
     localDate.year to localDate.monthValue
 } catch (_: Exception) { 0 to 0 }
-
-/**
- * yyyy-mm-dd -> MMddyyyy
- */
-fun String.toFormattedMonthDayYear(): String = try {
-    val parsedDate = LocalDate.parse(this, DateTimeFormatter.ISO_DATE)
-    parsedDate.format(DateTimeFormatter.ofPattern("MMddyyyy"))
-} catch (_: Exception) { this }
 
 /**
  * yyyyMMdd -> yyyy-MM-dd
