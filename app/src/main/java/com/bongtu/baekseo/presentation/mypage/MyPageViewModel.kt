72e66d579efbdd0889a63c12bc124ce916d54801
seungjun <jjw5741@naver.com>
2025-09-04 01:48:14 +0900

    [feat/#153] 마이페이지 조회API 연동

--- app/src/main/java/com/bongtu/baekseo/presentation/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/bongtu/baekseo/presentation/mypage/MyPageViewModel.kt
@@ -1,30 +1,44 @@
 package com.bongtu.baekseo.presentation.mypage
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bongtu.baekseo.core.common.state.UiState
 import com.bongtu.baekseo.core.common.type.IncomeType
 import com.bongtu.baekseo.core.util.TextFieldValidator.validateName
+import com.bongtu.baekseo.data.repository.profile.ProfileRepository
 import com.bongtu.baekseo.presentation.mypage.MyPageContract.MyPageUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MyPageViewModel @Inject constructor(
-
+    private val profileRepository: ProfileRepository,
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(MyPageUiState())
     val uiState = _uiState.asStateFlow()
 
-    fun fetchUserProfile() =        // TODO: 서버 통신 후 데이터 받아오기
-        _uiState.update { currentState ->
-            currentState.copy(
-                userName = "봉투백서의겸손한야수",
-                userBirth = "2000-01-05",
-                userIncome = IncomeType.OVER_200.label,
-            )
-        }
+    fun fetchUserProfile() = viewModelScope.launch {
+        updateMyPageRoadUiState(UiState.Loading)
+
+        profileRepository.getProfileInfo()
+            .onSuccess { response ->
+                _uiState.update {
+                    it.copy(
+                        userName = response.memberName,
+                        userBirth = response.memberBirthday,
+                        userIncome = IncomeType.getIncomeType(response.memberIncome).label,
+                    )
+                }
+                updateMyPageRoadUiState(UiState.Success(Unit))
+            }
+            .onFailure {
+                updateMyPageRoadUiState(UiState.Failure(it.message ?: "Unknown Error"))
+            }
+    }
 
     fun updateOriginProfileState(newName: String, newBirth: String, newIncome: String) =
         _uiState.update {
@@ -49,5 +63,9 @@ class MyPageViewModel @Inject constructor(
 
     fun updateUserIncome(newIncome: String) = _uiState.update {
         it.copy(userIncome = newIncome)
+    }
+
+    fun updateMyPageRoadUiState(newUiState: UiState<Unit>) = _uiState.update {
+        it.copy(myPageLoadState = newUiState)
     }
 }
