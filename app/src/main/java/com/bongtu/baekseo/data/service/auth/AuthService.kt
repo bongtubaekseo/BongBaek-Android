9d6a0c5645acbe7b87207bb5a911146e444b5c9f
Hyejeong <myejeong0211@gmail.com>
2025-09-03 21:00:08 +0900

    [feat/#150] 회원 탈퇴 api service 추가

--- app/src/main/java/com/bongtu/baekseo/data/service/auth/AuthService.kt
+++ app/src/main/java/com/bongtu/baekseo/data/service/auth/AuthService.kt
@@ -6,6 +6,7 @@ import com.bongtu.baekseo.data.dto.auth.PostKakaoLoginResponse
 import com.bongtu.baekseo.data.dto.auth.PostSignUpRequest
 import com.bongtu.baekseo.data.dto.auth.PostTokenReissueRequest
 import com.bongtu.baekseo.data.dto.auth.PostTokenReissueResponse
+import com.bongtu.baekseo.data.dto.auth.PostWithdrawRequest
 import retrofit2.http.Body
 import retrofit2.http.Header
 import retrofit2.http.POST
@@ -29,5 +30,11 @@ interface AuthService {
     @POST("/api/v1/member/logout")
     suspend fun postLogout(
         @Header("Authorization") accessToken: String,
+    ): BaseResponse<Unit>
+
+    @POST("/api/v1/member/withdraw")
+    suspend fun postWithdraw(
+        @Header("Authorization") accessToken: String,
+        @Body request: PostWithdrawRequest,
     ): BaseResponse<Unit>
 }
