106dc0b1c45d06529225f12d7f1c300ff272701c
Hyejeong <myejeong0211@gmail.com>
2025-09-03 21:00:33 +0900

    [add/#150] 회원 탈퇴 api usecase 추가

---
+++ app/src/main/java/com/bongtu/baekseo/domain/usecase/auth/WithdrawUseCase.kt
@@ -0,0 +1,24 @@
+package com.bongtu.baekseo.domain.usecase.auth
+
+import com.bongtu.baekseo.core.local.datastore.TokenDataStore
+import com.bongtu.baekseo.data.repository.auth.AuthRepository
+import javax.inject.Inject
+
+class WithdrawUseCase @Inject constructor(
+    private val tokenDataStore: TokenDataStore,
+    private val authRepository: AuthRepository,
+) {
+    suspend operator fun invoke(
+        withdrawalReason: String,
+        detail: String? = null,
+    ): Result<Unit> = runCatching {
+        val accessToken =
+            tokenDataStore.getAccessToken() ?: throw NoSuchElementException("accessToken 없음")
+
+        authRepository.postWithdraw(
+            accessToken = accessToken,
+            withdrawalReason = withdrawalReason,
+            detail = detail,
+        )
+    }
+}
