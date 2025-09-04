3103b685639337b36e035eb91dd04cff959bf858
seungjun <jjw5741@naver.com>
2025-09-04 20:45:39 +0900

    [mod/#153] MemberDataSource 추상체, 구현체 수정

--- app/src/main/java/com/bongtu/baekseo/data/datasource/member/MemberDataSource.kt
+++ app/src/main/java/com/bongtu/baekseo/data/datasource/member/MemberDataSource.kt
@@ -1,12 +1,20 @@
 package com.bongtu.baekseo.data.datasource.member
 
+import com.bongtu.baekseo.core.network.model.BaseResponse
 import com.bongtu.baekseo.core.network.model.BaseResponseWithoutData
 import com.bongtu.baekseo.data.dto.member.PostWithdrawRequest
+import com.bongtu.baekseo.data.dto.profile.ProfileInfoDto
 
 interface MemberDataSource {
     suspend fun postLogout(): BaseResponseWithoutData
 
     suspend fun postWithdraw(
         request: PostWithdrawRequest,
+    ): BaseResponseWithoutData
+
+    suspend fun getProfileInfo(): BaseResponse<ProfileInfoDto>
+
+    suspend fun putProfileInfo(
+        request: ProfileInfoDto,
     ): BaseResponseWithoutData
 }
--- app/src/main/java/com/bongtu/baekseo/data/datasourceimpl/member/MemberDataSourceImpl.kt
+++ app/src/main/java/com/bongtu/baekseo/data/datasourceimpl/member/MemberDataSourceImpl.kt
@@ -2,6 +2,7 @@ package com.bongtu.baekseo.data.datasourceimpl.member
 
 import com.bongtu.baekseo.data.datasource.member.MemberDataSource
 import com.bongtu.baekseo.data.dto.member.PostWithdrawRequest
+import com.bongtu.baekseo.data.dto.profile.ProfileInfoDto
 import com.bongtu.baekseo.data.service.member.MemberService
 import javax.inject.Inject
 
@@ -15,4 +16,10 @@ class MemberDataSourceImpl @Inject constructor(
     override suspend fun postWithdraw(
         request: PostWithdrawRequest,
     ) = memberService.postWithdraw(request)
+
+    override suspend fun getProfileInfo() = memberService.getProfileInfo()
+
+    override suspend fun putProfileInfo(
+        request: ProfileInfoDto,
+    ) = memberService.putProfileInfo(request)
 }
