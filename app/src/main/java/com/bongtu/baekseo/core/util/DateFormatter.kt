4fc9638282f76265ed8322da952c369b0bcd64c1
seungjun <jjw5741@naver.com>
2025-09-05 19:23:04 +0900

    [refactor/#154] StringExt 파일 삭제 및 DateFormatter로 통합

--- app/src/main/java/com/bongtu/baekseo/core/util/DateFormatter.kt
+++ app/src/main/java/com/bongtu/baekseo/core/util/DateFormatter.kt
@@ -2,24 +2,31 @@ package com.bongtu.baekseo.core.util
 
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
-import java.time.format.DateTimeParseException
 
 object DateFormatter {
 
-    private val INPUT_DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    private val LOCAL_DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd")
     private val KOREAN_DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일")
+    private val NUMERIC_DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMdd")
 
     fun formatToKorean(dateString: String): String = try {
-        val date = LocalDate.parse(dateString, INPUT_DATE_FORMATTER)
+        val date = LocalDate.parse(dateString, LOCAL_DATE_FORMATTER)
         date.format(KOREAN_DATE_FORMATTER)
-    } catch (e: DateTimeParseException) {
+    } catch (_: Exception) {
         "-"
     }
 
     fun getYearMonth(dateString: String): Pair<Int, Int> = try {
-        val date = LocalDate.parse(dateString, INPUT_DATE_FORMATTER)
+        val date = LocalDate.parse(dateString, LOCAL_DATE_FORMATTER)
         date.year to date.monthValue
     } catch (_: Exception) {
         0 to 0
+    }
+
+    fun formatNumericToLocalDate(dateString: String): String = try {
+        val date = LocalDate.parse(dateString, NUMERIC_DATE_FORMATTER)
+        date.format(LOCAL_DATE_FORMATTER)
+    } catch (_: Exception) {
+        ""
     }
 }
