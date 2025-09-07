6dad3cad8073338b6e96414d262d52489cf4ffeb
seungjun <jjw5741@naver.com>
2025-09-07 14:41:34 +0900

    [add/#168] URL 싱글톤 생성

---
+++ app/src/main/java/com/bongtu/baekseo/core/util/UrlConstant.kt
@@ -0,0 +1,7 @@
+package com.bongtu.baekseo.core.util
+
+object UrlConstant {
+    const val INQUIRY_URL = "https://www.notion.so/bongtubaekseo/264f06bb0d3480aa8badeba07a68b944"
+    const val TERMS_URL = "https://www.notion.so/bongtubaekseo/264f06bb0d348036b260f175a236ec7c"
+    const val PRIVACY_URL = "https://www.notion.so/bongtubaekseo/264f06bb0d3480d0b1eafa217b306105"
+}
