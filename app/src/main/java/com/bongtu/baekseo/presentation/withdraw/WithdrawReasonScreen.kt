c749a652d7756f1b2f61041bb52218b45e1838b4
Hyejeong <myejeong0211@gmail.com>
2025-09-14 20:16:59 +0900

    [fix/#171] 탈퇴하기 키보드 해제 시 포커스 해제

--- app/src/main/java/com/bongtu/baekseo/presentation/withdraw/WithdrawReasonScreen.kt
+++ app/src/main/java/com/bongtu/baekseo/presentation/withdraw/WithdrawReasonScreen.kt
@@ -9,17 +9,20 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
@@ -89,7 +92,16 @@ private fun WithdrawReasonScreen(
 ) {
     var isWithdrawDialogVisible by remember { mutableStateOf(false) }
     val focusManager = LocalFocusManager.current
+    val density = LocalDensity.current
+
+    val isImeVisible = WindowInsets.ime.getBottom(density) > 0
     var etcFocused by remember { mutableStateOf(false) }
+
+    LaunchedEffect(isImeVisible) {
+        if (!isImeVisible) {
+            focusManager.clearFocus()
+        }
+    }
 
     Column(
         modifier = modifier
