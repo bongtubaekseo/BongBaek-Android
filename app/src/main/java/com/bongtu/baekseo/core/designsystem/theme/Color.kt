47855f362d13091c4dad446a90ce1c234b50edc4
Jong <jm991014@gmail.com>
2025-11-15 18:49:25 +0900

    [add/#180] 새로운 컬러 시스템 추가
    light/dark 컬러 시스템 분리 및 테마에 isDarkTheme 추가

--- app/src/main/java/com/bongtu/baekseo/core/designsystem/theme/Color.kt
+++ app/src/main/java/com/bongtu/baekseo/core/designsystem/theme/Color.kt
@@ -13,131 +13,231 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.bongtu.baekseo.core.designsystem.theme.BongBaekTheme.colors
 
-// Primary
+// Global
+val white = Color(0xFFFFFFFF)
+val blacks = Color(0xFF000000)
+val charcoal = Color(0xFF3A3F4A)
+
+// Brand
 val primaryNormal = Color(0xFF6E7FFF)
 val primaryStrong = Color(0xFF502EFF)
 val primaryLight = Color(0xFFDFDCFB)
-val primaryGradient = Color(0xFF807FFF)
-val primaryBackground = Color(0x1A6E7FFF)
+val primaryBackground16 = Color(0x296E7FFF)
+val primaryBackground10 = Color(0x1A6E7FFF)
+val secondaryNormal = Color(0xFFFF9AA7)
+val secondaryStrong = Color(0xFFEA6D7D)
 
-// Secondary
-val secondaryRed = Color(0xFFFF9AA7)
+// GrayScale(Light)
+val light100 = Color(0xFFF5F7FA)
+val light150 = Color(0xFFF0F2F5)
+val light200 = Color(0xFFEBEDF0)
+val light300 = Color(0xFFDCDEE1)
+val light400 = Color(0xFFC4C5C8)
+val light500 = Color(0xFFABACAF)
+val light600 = Color(0xFF939496)
+val light700 = Color(0xFF494A4B)
+val light800 = Color(0xFF313132)
+val light900 = Color(0xFF181819)
 
-// Gray Scale
-val gray900 = Color(0xFF141516)
-val gray850 = Color(0xFF171922)
-val gray800 = Color(0xFF1B1C1D)
-val gray750 = Color(0xFF262835)
-val gray700 = Color(0xFF28292A)
-val gray600 = Color(0xFF3B3C3D)
-val gray500 = Color(0xFF4E5051)
-val gray400 = Color(0xFF717374)
-val gray300 = Color(0xFF949697)
-val gray200 = Color(0xFFB7B9BA)
-val gray100 = Color(0xFFDADBDB)
+// GrayScale(Dark)
+val dark100 = Color(0xFFDADBDB)
+val dark200 = Color(0xFFB7B9BA)
+val dark300 = Color(0xFF949697)
+val dark400 = Color(0xFF717374)
+val dark500 = Color(0xFF4E5051)
+val dark600 = Color(0xFF3B3C3D)
+val dark700 = Color(0xFF28292A)
+val dark750 = Color(0xFF262835)
+val dark800 = Color(0xFF1B1C1D)
+val dark900 = Color(0xFF141516)
 
-// Common
-val lineNormal = Color(0xFF3A3F4A)
-val white = Color(0xFFFFFFFF)
-val black = Color(0xFF000000)
-val transparent = Color(0x00000000)
+// Gradients
+val splashStart = Color(0xFF6E7FFF)
+val splashEnd = Color(0xFF6E7FFF)
+val envelopeStart = Color(0xFFA6BEF3)
+val envelopeEnd = Color(0xFFD3D9FF)
+val recommendResultStart = Color(0xFF4E62FF)
+val recordStart = Color(0xFF6648FF)
+val recordEnd = Color(0xFF6F53FF)
+
+// Others
 val kakaoYellow = Color(0xFFFEE500)
-val onboardingTitle = Color(0xFFCDC9FF)
-val pageIndicator = Color(0x4DFFFFFF)
-
-// Gradient
-val gradientSlider = Color(0xFF8B5CF6)
-val gradientEnvelopeStart = Color(0xE6A6BEF3)
-val gradientEnvelopeEnd = Color(0xE6D3D9FF)
-val gradientExpense = Color(0xFF4E62FF)
-val gradientCostCardBackGround = Color(0xFF6F53FF)
-val gradientCostCardBorder = Color(0xFFBFB8FF)
-val gradientCardStop1 = Color(0xFF150857)
-val gradientCardStop2 = Color(0xFF5F57FF)
-val gradientCardStop3 = Color(0xFF7384FF)
-val gradientCardStop4 = Color(0xFF9EA5FF)
-val gradientCardStop5 = Color(0xFFCDCBFF)
+val transparent = Color(0x00000000)
 
 @Immutable
 data class BongBaekColors(
-    val primaryNormal: Color,
-    val primaryStrong: Color,
-    val primaryLight: Color,
-    val primaryGradient: Color,
-    val primaryBackground: Color,
-    val secondaryRed: Color,
-    val gray900: Color,
-    val gray850: Color,
-    val gray800: Color,
-    val gray750: Color,
-    val gray700: Color,
-    val gray600: Color,
-    val gray500: Color,
-    val gray400: Color,
-    val gray300: Color,
-    val gray200: Color,
-    val gray100: Color,
-    val lineNormal: Color,
-    val white: Color,
-    val black: Color,
-    val transparent: Color,
+    val txtDisplayPrimary: Color,
+    val txtDisplaySecondary: Color,
+    val txtDisplayTertiary: Color,
+    val txtDisplaySubtle: Color,
+    val txtFieldValue: Color,
+    val txtFieldPlaceholder: Color,
+    val txtInteractiveInverse: Color,
+    val txtInteractivePrimary: Color,
+    val txtInteractiveSecondary: Color,
+    val txtStatusFocused: Color,
+    val txtStatusDisabled: Color,
+    val txtStatusError: Color,
+    val gnbDisplayBase: Color,
+    val bgDisplayPrimary: Color,
+    val bgDisplaySecondary: Color,
+    val bgDisplayCard: Color,
+    val bgDisplayChips: Color,
+    val bgDisplayRange: Color,
+    val bgFieldPrimary: Color,
+    val bgFieldSecondary: Color,
+    val bgStatusFocused: Color,
+    val btnInteractiveInput: Color,
+    val btnInteractivePrimary: Color,
+    val btnInteractiveSecondary: Color,
+    val btnInteractiveTertiary: Color,
+    val btnInteractiveSwitch: Color,
+    val btnInteractiveAccent: Color,
+    val btnInteractiveDisabled: Color,
+    val menuSelectedPrimary: Color,
+    val iconInteractiveDefault: Color,
+    val iconInteractiveKnob: Color,
+    val iconFocusedPrimary: Color,
+    val iconFocusedSecondary: Color,
+    val iconDisabledPrimary: Color,
+    val iconDisabledSecondary: Color,
+    val iconStatusError: Color,
+    val borderDisplayTitle: Color,
+    val borderDividerPrimary: Color,
+    val borderFieldDefault: Color,
+    val borderFieldFilled: Color,
+    val borderInteractiveSwitch: Color,
+    val borderStatusFocused: Color,
+    val borderStatusError: Color,
+    val splashStart: Color,
+    val splashEnd: Color,
+    val envelopeStart: Color,
+    val envelopeEnd: Color,
+    val recommendResultStart: Color,
+    val recordEnd: Color,
+    val recordStart: Color,
     val kakaoYellow: Color,
-    val onboardingTitle: Color,
-    val pageIndicator: Color,
-    val gradientSlider: Color,
-    val gradientEnvelopeStart: Color,
-    val gradientEnvelopeEnd: Color,
-    val gradientExpense: Color,
-    val gradientCostCardBackGround: Color,
-    val gradientCostCardBorder: Color,
-    val gradientCardStop1: Color,
-    val gradientCardStop2: Color,
-    val gradientCardStop3: Color,
-    val gradientCardStop4: Color,
-    val gradientCardStop5: Color,
+    val blacks: Color,
+    val transparent: Color,
+    val isLight: Boolean,
 )
 
-val defaultBongBaekColors = BongBaekColors(
-    primaryNormal = primaryNormal,
-    primaryStrong = primaryStrong,
-    primaryLight = primaryLight,
-    primaryGradient = primaryGradient,
-    primaryBackground = primaryBackground,
-    secondaryRed = secondaryRed,
-    gray900 = gray900,
-    gray850 = gray850,
-    gray800 = gray800,
-    gray750 = gray750,
-    gray700 = gray700,
-    gray600 = gray600,
-    gray500 = gray500,
-    gray400 = gray400,
-    gray300 = gray300,
-    gray200 = gray200,
-    gray100 = gray100,
-    lineNormal = lineNormal,
-    white = white,
-    black = black,
-    transparent = transparent,
+val lightBongBaekColors = BongBaekColors(
+    txtDisplayPrimary = blacks,
+    txtDisplaySecondary = light900,
+    txtDisplayTertiary = light600,
+    txtDisplaySubtle = primaryNormal,
+    txtFieldValue = blacks,
+    txtFieldPlaceholder = light600,
+    txtInteractiveInverse = white,
+    txtInteractivePrimary = light800,
+    txtInteractiveSecondary = light700,
+    txtStatusFocused = primaryNormal,
+    txtStatusDisabled = light500,
+    txtStatusError = secondaryStrong,
+    gnbDisplayBase = light150,
+    bgDisplayPrimary = white,
+    bgDisplaySecondary = light100,
+    bgDisplayCard = light200,
+    bgDisplayChips = primaryBackground16,
+    bgDisplayRange = light300,
+    bgFieldPrimary = light100,
+    bgFieldSecondary = light200,
+    bgStatusFocused = primaryNormal,
+    btnInteractiveInput = white,
+    btnInteractivePrimary = light100,
+    btnInteractiveSecondary = light200,
+    btnInteractiveTertiary = light200,
+    btnInteractiveSwitch = light300,
+    btnInteractiveAccent = primaryStrong,
+    btnInteractiveDisabled = primaryBackground16,
+    menuSelectedPrimary = light700,
+    iconInteractiveDefault = light400,
+    iconInteractiveKnob = white,
+    iconFocusedPrimary = primaryNormal,
+    iconFocusedSecondary = primaryLight,
+    iconDisabledPrimary = light500,
+    iconDisabledSecondary = light400,
+    iconStatusError = secondaryStrong,
+    borderDisplayTitle = light100,
+    borderDividerPrimary = light300,
+    borderFieldDefault = light300,
+    borderFieldFilled = light500,
+    borderInteractiveSwitch = light200,
+    borderStatusFocused = primaryNormal,
+    borderStatusError = secondaryStrong,
+    splashStart = splashStart,
+    splashEnd = splashEnd,
+    envelopeStart = envelopeStart,
+    envelopeEnd = envelopeEnd,
+    recommendResultStart = recommendResultStart,
+    recordStart = recordStart,
+    recordEnd = recordEnd,
     kakaoYellow = kakaoYellow,
-    onboardingTitle = onboardingTitle,
-    pageIndicator = pageIndicator,
-    gradientSlider = gradientSlider,
-    gradientEnvelopeStart = gradientEnvelopeStart,
-    gradientEnvelopeEnd = gradientEnvelopeEnd,
-    gradientExpense = gradientExpense,
-    gradientCostCardBackGround = gradientCostCardBackGround,
-    gradientCostCardBorder = gradientCostCardBorder,
-    gradientCardStop1 = gradientCardStop1,
-    gradientCardStop2 = gradientCardStop2,
-    gradientCardStop3 = gradientCardStop3,
-    gradientCardStop4 = gradientCardStop4,
-    gradientCardStop5 = gradientCardStop5,
+    blacks = blacks,
+    transparent = transparent,
+    isLight = true,
 )
 
-val LocalBongBaekColors = staticCompositionLocalOf { defaultBongBaekColors }
+val darkBongBaekColors = BongBaekColors(
+    txtDisplayPrimary = white,
+    txtDisplaySecondary = dark100,
+    txtDisplayTertiary = dark400,
+    txtDisplaySubtle = primaryLight,
+    txtFieldValue = white,
+    txtFieldPlaceholder = dark400,
+    txtInteractiveInverse = white,
+    txtInteractivePrimary = dark100,
+    txtInteractiveSecondary = dark200,
+    txtStatusFocused = primaryNormal,
+    txtStatusDisabled = dark500,
+    txtStatusError = secondaryNormal,
+    gnbDisplayBase = dark700,
+    bgDisplayPrimary = dark900,
+    bgDisplaySecondary = dark800,
+    bgDisplayCard = dark750,
+    bgDisplayChips = primaryBackground10,
+    bgDisplayRange = dark500,
+    bgFieldPrimary = dark800,
+    bgFieldSecondary = dark750,
+    bgStatusFocused = primaryNormal,
+    btnInteractiveInput = primaryBackground10,
+    btnInteractivePrimary = dark800,
+    btnInteractiveSecondary = dark700,
+    btnInteractiveTertiary = dark750,
+    btnInteractiveSwitch = dark400,
+    btnInteractiveAccent = primaryStrong,
+    btnInteractiveDisabled = primaryBackground10,
+    menuSelectedPrimary = white,
+    iconInteractiveDefault = white,
+    iconInteractiveKnob = white,
+    iconFocusedPrimary = primaryNormal,
+    iconFocusedSecondary = primaryLight,
+    iconDisabledPrimary = dark500,
+    iconDisabledSecondary = dark400,
+    iconStatusError = secondaryNormal,
+    borderDisplayTitle = blacks,
+    borderDividerPrimary = charcoal,
+    borderFieldDefault = charcoal,
+    borderFieldFilled = white,
+    borderInteractiveSwitch = dark100,
+    borderStatusFocused = primaryNormal,
+    borderStatusError = secondaryNormal,
+    splashStart = splashStart,
+    splashEnd = splashEnd,
+    envelopeStart = envelopeStart,
+    envelopeEnd = envelopeEnd,
+    recommendResultStart = recommendResultStart,
+    recordStart = recordStart,
+    recordEnd = recordEnd,
+    kakaoYellow = kakaoYellow,
+    blacks = blacks,
+    transparent = transparent,
+    isLight = false,
+)
+
+val LocalBongBaekColors = staticCompositionLocalOf { lightBongBaekColors }
 
 @Preview(showBackground = true)
 @Composable
@@ -145,42 +245,46 @@ private fun BongBaekColorsPreview() {
     BongBaekTheme {
         Column {
             listOf(
-                colors.primaryNormal,
-                colors.primaryStrong,
-                colors.primaryLight,
-                colors.primaryGradient,
-                colors.primaryBackground,
-                colors.secondaryRed,
-                colors.gray900,
-                colors.gray850,
-                colors.gray800,
-                colors.gray750,
-                colors.gray700,
-                colors.gray600,
-                colors.gray500,
-                colors.gray400,
-                colors.gray300,
-                colors.gray200,
-                colors.gray100,
-                colors.lineNormal,
-                colors.white,
-                colors.black,
-                colors.transparent,
-                colors.kakaoYellow,
-                colors.onboardingTitle,
-                colors.pageIndicator,
-                colors.gradientSlider,
-                colors.gradientEnvelopeStart,
-                colors.gradientEnvelopeEnd,
-                colors.gradientExpense,
-                colors.gradientCostCardBackGround,
-                colors.gradientCostCardBorder,
-                colors.gradientCardStop1,
-                colors.gradientCardStop2,
-                colors.gradientCardStop3,
-                colors.gradientCardStop4,
-                colors.gradientCardStop5,
-            ).chunked(4).forEach { color ->
+                white,
+                blacks,
+                charcoal,
+                primaryNormal,
+                primaryStrong,
+                primaryLight,
+                primaryBackground16,
+                primaryBackground10,
+                secondaryNormal,
+                secondaryStrong,
+                light100,
+                light150,
+                light200,
+                light300,
+                light400,
+                light500,
+                light600,
+                light700,
+                light800,
+                light900,
+                dark100,
+                dark200,
+                dark300,
+                dark400,
+                dark500,
+                dark600,
+                dark700,
+                dark750,
+                dark800,
+                dark900,
+                splashStart,
+                splashEnd,
+                envelopeStart,
+                envelopeEnd,
+                recommendResultStart,
+                recordStart,
+                recordEnd,
+                kakaoYellow,
+                transparent,
+            ).chunked(6).forEach { color ->
                 Row(
                     modifier = Modifier.padding(vertical = 4.dp),
                 ) {
