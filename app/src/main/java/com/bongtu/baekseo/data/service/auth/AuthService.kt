43a34fa6c03c9598bbc3b68303262ed0bd0fe31c
seungjun <jjw5741@naver.com>
2025-11-17 22:15:11 +0900

    [feat/#194] social service 구현

--- app/src/main/java/com/bongtu/baekseo/data/service/auth/AuthService.kt
+++ app/src/main/java/com/bongtu/baekseo/data/service/auth/AuthService.kt
@@ -4,12 +4,19 @@ import com.bongtu.baekseo.core.network.model.BaseResponse
 import com.bongtu.baekseo.data.dto.auth.PostKakaoLoginRequest
 import com.bongtu.baekseo.data.dto.auth.PostKakaoLoginResponse
 import com.bongtu.baekseo.data.dto.auth.PostSignUpRequest
+import com.bongtu.baekseo.data.dto.auth.PostSocialLoginRequest
+import com.bongtu.baekseo.data.dto.auth.PostSocialLoginResponse
 import com.bongtu.baekseo.data.dto.auth.PostTokenReissueRequest
 import com.bongtu.baekseo.data.dto.auth.PostTokenReissueResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthService {
+    @POST("/api/v1/oauth/{oauthProvider}")
+    suspend fun postSocialLogin(
+        @Body request: PostSocialLoginRequest,
+    ): BaseResponse<PostSocialLoginResponse>
+
     @POST("/api/v1/oauth/kakao")
     suspend fun postKakaoLogin(
         @Body request: PostKakaoLoginRequest,
