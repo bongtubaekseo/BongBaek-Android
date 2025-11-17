c50c3c8c6bbbd5c781a1d9ec3e76af34475f15d1
seungjun <jjw5741@naver.com>
2025-11-17 22:14:37 +0900

    [feat/#194] social datasource 구현

--- app/src/main/java/com/bongtu/baekseo/data/datasource/auth/AuthDataSource.kt
+++ app/src/main/java/com/bongtu/baekseo/data/datasource/auth/AuthDataSource.kt
@@ -4,10 +4,16 @@ import com.bongtu.baekseo.core.network.model.BaseResponse
 import com.bongtu.baekseo.data.dto.auth.PostKakaoLoginRequest
 import com.bongtu.baekseo.data.dto.auth.PostKakaoLoginResponse
 import com.bongtu.baekseo.data.dto.auth.PostSignUpRequest
+import com.bongtu.baekseo.data.dto.auth.PostSocialLoginRequest
+import com.bongtu.baekseo.data.dto.auth.PostSocialLoginResponse
 import com.bongtu.baekseo.data.dto.auth.PostTokenReissueRequest
 import com.bongtu.baekseo.data.dto.auth.PostTokenReissueResponse
 
 interface AuthDataSource {
+    suspend fun postSocialLogin(
+        request: PostSocialLoginRequest,
+    ): BaseResponse<PostSocialLoginResponse>
+
     suspend fun postKakaoLogin(
         request: PostKakaoLoginRequest,
     ): BaseResponse<PostKakaoLoginResponse>
--- app/src/main/java/com/bongtu/baekseo/data/datasourceimpl/auth/AuthDataSourceImpl.kt
+++ app/src/main/java/com/bongtu/baekseo/data/datasourceimpl/auth/AuthDataSourceImpl.kt
@@ -3,6 +3,7 @@ package com.bongtu.baekseo.data.datasourceimpl.auth
 import com.bongtu.baekseo.data.datasource.auth.AuthDataSource
 import com.bongtu.baekseo.data.dto.auth.PostKakaoLoginRequest
 import com.bongtu.baekseo.data.dto.auth.PostSignUpRequest
+import com.bongtu.baekseo.data.dto.auth.PostSocialLoginRequest
 import com.bongtu.baekseo.data.dto.auth.PostTokenReissueRequest
 import com.bongtu.baekseo.data.service.auth.AuthService
 import javax.inject.Inject
@@ -10,6 +11,9 @@ import javax.inject.Inject
 class AuthDataSourceImpl @Inject constructor(
     private val authService: AuthService,
 ) : AuthDataSource {
+    override suspend fun postSocialLogin(request: PostSocialLoginRequest) =
+        authService.postSocialLogin(request)
+
     override suspend fun postKakaoLogin(request: PostKakaoLoginRequest) =
         authService.postKakaoLogin(request)
 
