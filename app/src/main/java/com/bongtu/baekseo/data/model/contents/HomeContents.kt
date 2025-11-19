a97f6e6fc3cc441098b0896bb957a8f74dc3def6
Hyejeong <myejeong0211@gmail.com>
2025-11-19 20:37:32 +0900

    [add/#191] 홈 컨텐츠 data class 추가

---
+++ app/src/main/java/com/bongtu/baekseo/data/model/contents/HomeContents.kt
@@ -0,0 +1,8 @@
+package com.bongtu.baekseo.data.model.contents
+
+data class HomeContents(
+    val contentId: String,
+    val contentTitle: String,
+    val contentCategory: String,
+    val thumbnailUrl: String,
+)
