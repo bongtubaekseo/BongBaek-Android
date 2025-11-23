0a84ba8b061b889bf826e198763510cd21dc1735
seungjun <jjw5741@naver.com>
2025-11-23 22:34:42 +0900

    [refactor/#203] 기록하기 드롭다운 타입 추가

---
+++ app/src/main/java/com/bongtu/baekseo/presentation/detail/type/DetailDropDownItemType.kt
@@ -0,0 +1,23 @@
+package com.bongtu.baekseo.presentation.detail.type
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.runtime.Immutable
+
+data class DetailDropDownItemType(
+    @DrawableRes val iconRes: Int,
+    @StringRes val labelRes: Int,
+    val trailingType: DetailDropDownTrailingType? = null,
+)
+
+@Immutable
+sealed interface DetailDropDownTrailingType {
+    @Immutable
+    data class TrailingText(val text: String) : DetailDropDownTrailingType
+
+    @Immutable
+    data class TrailingCost(val text: String) : DetailDropDownTrailingType
+
+    @Immutable
+    data class TrailingChip(val text: String) : DetailDropDownTrailingType
+}
