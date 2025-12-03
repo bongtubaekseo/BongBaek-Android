2911bc73b9107d555d650ef3f2a701ba31495656
seungjun <jjw5741@naver.com>
2025-12-03 10:03:31 +0900

    [del/#215] ProfileSetting 기존 ProfileEdit 코드 제거

--- app/src/main/java/com/bongtu/baekseo/presentation/setting/SettingContract.kt
+++ app/src/main/java/com/bongtu/baekseo/presentation/setting/SettingContract.kt
@@ -9,33 +9,11 @@ class SettingContract {
     data class SettingUiState(
         val loadState: UiState<Unit> = UiState.Empty,
         val userName: String = "",
-        val nameError: String = "",
         val userBirth: String = "",
-        val dialogBirth: String = "",
         val userIncome: IncomeType = IncomeType.NONE,
-        val originalName: String = "",
-        val originalBirth: String = "",
-        val originalIncome: IncomeType = IncomeType.NONE,
-    ) {
-        val isFormValid: Boolean
-            get() = userName.isNotEmpty() && userBirth.isNotEmpty() && nameError.isEmpty()
+    )
 
-        val isProfileChanged: Boolean
-            get() = userName != originalName ||
-                    userBirth != originalBirth ||
-                    userIncome != originalIncome
-
-        val isEditButtonEnabled: Boolean
-            get() = isFormValid && isProfileChanged && loadState !is UiState.Loading
-    }
-
-    sealed interface SettingSideEffect {
-        sealed class MainSideEffect : SettingSideEffect {
-            data object RestartApp : MainSideEffect()
-        }
-
-        sealed class ProfileEditSideEffect : SettingSideEffect {
-            data object NavigateToSetting : ProfileEditSideEffect()
-        }
+    sealed class SettingSideEffect {
+        data object RestartApp : SettingSideEffect()
     }
 }
--- app/src/main/java/com/bongtu/baekseo/presentation/setting/navigation/SettingNavigation.kt
+++ app/src/main/java/com/bongtu/baekseo/presentation/setting/navigation/SettingNavigation.kt
@@ -3,62 +3,31 @@ package com.bongtu.baekseo.presentation.setting.navigation
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavHostController
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.navigation
 import com.bongtu.baekseo.core.common.navigation.MainTabRoute
-import com.bongtu.baekseo.core.common.navigation.Route
-import com.bongtu.baekseo.core.util.sharedViewModel
-import com.bongtu.baekseo.presentation.setting.ProfileEditRoute
+import com.bongtu.baekseo.core.common.type.IncomeType
 import com.bongtu.baekseo.presentation.setting.SettingRoute
-import com.bongtu.baekseo.presentation.setting.SettingViewModel
 import kotlinx.serialization.Serializable
 
 fun NavController.navigateToSetting(navOptions: NavOptions? = null) =
     navigate(Setting, navOptions)
 
-fun NavController.navigateToProfileEdit(navOptions: NavOptions? = null) =
-    navigate(ProfileEdit, navOptions)
-
 fun NavGraphBuilder.settingGraph(
-    navController: NavHostController,
+    navigateToProfileEdit: (String, String, IncomeType) -> Unit,
     navigateToWithdraw: () -> Unit,
     onRestartApp: (Boolean) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    navigation<Setting>(
-        startDestination = SettingMain,
-    ) {
-        composable<SettingMain> { backStackEntry ->
-            val viewModel = backStackEntry.sharedViewModel<SettingViewModel>(navController)
-
-            SettingRoute(
-                navigateToEditProfile = navController::navigateToProfileEdit,
-                navigateToWithdraw = navigateToWithdraw,
-                onRestartApp = onRestartApp,
-                viewModel = viewModel,
-                modifier = modifier,
-            )
-        }
-
-        composable<ProfileEdit> { backStackEntry ->
-            val viewModel = backStackEntry.sharedViewModel<SettingViewModel>(navController)
-
-            ProfileEditRoute(
-                navigateToUp = navController::navigateUp,
-                viewModel = viewModel,
-                modifier = modifier,
-            )
-        }
+    composable<Setting> {
+        SettingRoute(
+            navigateToEditProfile = navigateToProfileEdit,
+            navigateToWithdraw = navigateToWithdraw,
+            onRestartApp = onRestartApp,
+            modifier = modifier,
+        )
     }
 }
 
 @Serializable
-data object Setting : Route
-
-@Serializable
-data object SettingMain : MainTabRoute
-
-@Serializable
-data object ProfileEdit : Route
+data object Setting : MainTabRoute
