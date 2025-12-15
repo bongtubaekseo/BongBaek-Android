9ecc7843cfe5908fb1fd888b9a4c4da4a537e3f9
Jong <jm991014@gmail.com>
2025-12-15 20:48:17 +0900

    Removes unnecessary system bar padding

--- app/src/main/java/com/bongtu/baekseo/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/bongtu/baekseo/presentation/login/LoginScreen.kt
@@ -8,10 +8,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -198,8 +202,7 @@ fun LoginScreen(
         modifier = modifier
             .fillMaxSize()
             .background(color = BongBaekTheme.colors.bgDisplaySecondary)
-            .padding(horizontal = 20.dp)
-            .systemBarsPadding(),
+            .padding(horizontal = 20.dp),
         verticalArrangement = Arrangement.SpaceBetween,
     ) {
         Column(
